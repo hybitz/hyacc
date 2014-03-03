7d6380445972dbe9e1fc0da7b406a3bc8b231149
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2014-03-03 10:49:43 +0900

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :use_sudo, false
 set :application, "hyacc"
 set :repository,  "git://github.com/hybitz/hyacc.git"
 set :scm, :git
-set :branch, 'master'
+set :branch, 'release'
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`

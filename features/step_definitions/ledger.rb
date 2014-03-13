e022888cde1e0f7eb7f670b5cc1d24991a6f66d3
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2014-03-13 13:09:32 +0900

    テスト

--- features/step_definitions/ledger.rb
+++ features/step_definitions/ledger.rb
@@ -3,3 +3,13 @@
 ならば /^元帳に遷移する$/ do
   assert_url '/ledger'
 end
+
+もし /^勘定科目に(.*?)を選択して表示する$/ do |account_name|
+  assert @account = Account.find_by_name(account_name)
+  select @account.code_and_name, :from => 'finder_account_id'
+  click_on '表示'
+end
+
+ならば /^現金仕訳の月別累計が表示される$/ do
+  assert_url '/ledger'
+end

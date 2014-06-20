4b245c17c1acb570619fc5a52c2a8491f199df89
ichy <ichylinux@gmail.com>
2014-06-20 16:56:42 +0900

    デザイン

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,13 +1,5 @@
-# -*- encoding : utf-8 -*-
-#
-# $Id: report_controller.rb 2467 2011-03-23 14:56:39Z ichy $
-# Product: hyacc
-# Copyright 2009 by Hybitz.co.ltd
-# ALL Rights Reserved.
-#
 class ReportController < Base::HyaccController
-  layout '_menu'
-  view_attribute :title=>'帳票出力'
+  view_attribute :title => '帳票出力'
 
   def index
   end

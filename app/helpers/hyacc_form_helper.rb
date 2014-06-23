79a85c04050416c369ab2e85edecd33fe71b17cc
ichy <ichylinux@gmail.com>
2014-06-24 00:09:17 +0900

    リファクタリング

--- app/helpers/hyacc_form_helper.rb
+++ app/helpers/hyacc_form_helper.rb
@@ -1,10 +1,6 @@
 module HyaccFormHelper
   include HyaccViewHelper
   
-  def link_to_journal(name, jh)
-    link_to(name, {:controller => :journal, :action => :show, :id => jh}, :remote => true)
-  end
-
   def h_account_select(object_name, field_name, options={})
     clazz = options.has_key?(:class) ? options[:class] : 'accountSelect'
     
@@ -22,26 +18,10 @@ module HyaccFormHelper
       {:class=>'branchSelect', :style => options[:style]} )
   end
 
-  def h_dc_type_select(object_name, options={})
-    select(object_name, "dc_type", dc_types,
-      {:include_blank => include_blank(options) },
-      {:class=>'dcTypeSelect'} ) 
-  end
-
   def h_sub_account_select(object_name, options={})
     collection_select(object_name, "sub_account_id", @sub_accounts, "id", "name",
       {:include_blank=>include_blank(options)},
       {:class=>'subAccountSelect', :style => options[:style]})
-  end
-
-  def h_tax_type_select(object_name, options={})
-    select(object_name, "tax_type", tax_types,
-      {:include_blank => include_blank(options) },
-      {:class=>'taxTypeSelect'} ) 
-  end
-  
-  def h_amount_field(object_name, options={})
-    text_field(object_name, property_name(options, 'amount'), {:class=>'amountText'})
   end
 
   def collection_select_code_and_name( *args )
@@ -84,8 +64,4 @@ module HyaccFormHelper
     false
   end
 
-  def property_name(options, default_name)
-    return options[:property_name] if options.has_key? :property_name
-    default_name
-  end
 end

7473300ac52b631252f7c1329219e03154b85e5f
ichy <ichylinux@gmail.com>
2014-06-23 23:05:44 +0900

    リファクタリング

--- app/controllers/journal_admin_controller.rb
+++ app/controllers/journal_admin_controller.rb
@@ -1,12 +1,4 @@
-# coding: UTF-8
-#
-# $Id: journal_admin_controller.rb 3088 2013-07-17 12:53:36Z ichy $
-# Product: hyacc
-# Copyright 2009-2013 by Hybitz.co.ltd
-# ALL Rights Reserved.
-#
 class JournalAdminController < Base::HyaccController
-  layout '_menu'
   view_attribute :title => '仕訳管理'
 
   def index

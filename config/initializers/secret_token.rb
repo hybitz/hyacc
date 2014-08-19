47c512be0cf68999d93f3289063327eb36c9623a
ichy <ichylinux@gmail.com>
2014-08-20 01:53:23 +0900

    refs #234

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hyacc::Application.config.secret_key_base = '1cbb79491ebab33759e6caf563987bbd50fea4624650069a18e7c0357320a3e6d6e9b243dcc1c25a4371f6ad10267a62d972a851808f59ea283f755cc423d572'

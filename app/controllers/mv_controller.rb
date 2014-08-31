aeb0a1b16b32f2ff71f63d8c6d5680a7c233d9a0
ichy <ichylinux@gmail.com>
2014-08-31 17:17:25 +0900

    リファクタリング

--- app/controllers/mv_controller.rb
+++ app/controllers/mv_controller.rb
@@ -1,10 +1,3 @@
-# -*- encoding : utf-8 -*-
-#
-# $Id: mv_controller.rb 2617 2011-08-19 05:27:10Z hiro $
-# Product: hyacc
-# Copyright 2009 by Hybitz.co.ltd
-# ALL Rights Reserved.
-#
 class MvController < Base::HyaccController
   layout '_menu'
   view_attribute :title=>'マスタビューア'

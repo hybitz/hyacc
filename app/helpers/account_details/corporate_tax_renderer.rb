50ccc13ef10a5872c18369b75d0f3e44b3e17741
ichy <ichylinux@gmail.com>
2014-09-08 20:15:09 +0900

    リファクタリング

--- app/helpers/account_details/corporate_tax_renderer.rb
+++ app/helpers/account_details/corporate_tax_renderer.rb
@@ -1,10 +1,3 @@
-# -*- encoding : utf-8 -*-
-#
-# $Id: corporate_tax_renderer.rb 2470 2011-03-23 14:58:00Z ichy $
-# Product: hyacc
-# Copyright 2010 by Hybitz.co.ltd
-# ALL Rights Reserved.
-#
 module AccountDetails
 
   class CorporateTaxRenderer < AccountDetailRenderer

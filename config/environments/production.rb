e0c89156eaba99ac91cdbf470e1255647aa91e6c
ichy <ichylinux@gmail.com>
2014-10-17 17:22:09 +0900

    デフォルトスキームの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Hyacc::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { :host => 'hyacc.reading.jp' }
+  config.action_mailer.default_url_options = {:scheme => 'https', :host => 'hyacc.reading.jp' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).

1ccc3a5eb45641ef42172f9c4111b9d6d616dd20
hiro <hiroyuki@hybitz.co.jp>
2014-10-17 17:21:44 +0900

    スキーマのマージ

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20141017062852) do
-=======
-ActiveRecord::Schema.define(version: 20141017062157) do
->>>>>>> branch 'master' of git@github.com:hybitz/hyacc.git
 
   create_table "account_controls", force: true do |t|
     t.integer  "account_id",                           null: false

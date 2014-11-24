3e4f59d05aed88097698616e9ce809746364d25e
ichy <ichylinux@gmail.com>
2014-11-24 12:03:31 +0900

    デバッグ

--- config/initializers/gcalapi.rb
+++ config/initializers/gcalapi.rb
@@ -1,33 +1,25 @@
-# -*- encoding : utf-8 -*-
 require 'googlecalendar/service'
 require 'googlecalendar/event'
 require 'time'
 
 module GoogleCalendar
-  class Service
-  private 
-    def auth
-      https = Net::HTTP.new(AUTH_SERVER, 443, @@proxy_addr, @@proxy_port, @@proxy_user, @@proxy_pass)
-      https.use_ssl = true
-      https.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      head = {'Content-Type' => 'application/x-www-form-urlencoded'}
-      logger.info "-- auth st --" if logger
-      https.start do |w|
-        res = w.post(AUTH_PATH, "Email=#{@email}&Passwd=#{CGI.escape(@pass)}&source=company-app-1&service=cl&accountType=HOSTED", head)
-        logger.debug res if logger
-        if res.body =~ /Auth=(.+)/
-          @auth = $1 
-        else
-          if logger
-            logger.fatal(res)
-            logger.fatal(res.body)
-          end
-          raise AuthenticationFailed
+
+  class Calendar
+    def self.calendars(srv)
+      ret = srv.calendar_list
+      Rails.logger.debug ret.to_yaml
+      list = REXML::Document.new(ret.body)
+      h = {}
+      list.root.elements.each("entry/link") do |e|
+        if e.attributes["rel"] == "alternate"
+          feed = e.attributes["href"]
+          h[feed] = Calendar.new(srv, feed)
         end
       end
+      h
     end
   end
-  
+
   class Event
     attr_accessor :id, :who, :recurrence
     ATTRIBUTES_MAP["id"] = {"element" => "id"}
@@ -38,4 +30,5 @@ module GoogleCalendar
       true
     end
   end
+
 end

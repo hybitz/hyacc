3fdb591c785dd4e0af33547e642bbe25a14ea23c
hiro <hiroyuki@hybitz.co.jp>
2014-12-12 16:42:20 +0900

    配偶者控除有無の表示

--- app/models/reports/withholding_detail_logic.rb
+++ app/models/reports/withholding_detail_logic.rb
@@ -8,17 +8,21 @@ module Reports
     end
     
     def get_withholding_info
+      e = get_exemptions
       model = WithholdingDetail.new
       model.calendar_year = @finder.calendar_year
       model.company = Company.find(@finder.company_id)
       model.head_business_office = model.company.get_head_business_office
       model.employee = Employee.find(@finder.employee_id)
       model.total_salary = get_total_salary                               # 支払金額
-      model.exemption = get_exemption                                     # 所得控除の額の合計
+      model.exemption = get_total_exemption                                     # 所得控除の額の合計
       model.after_deduction = get_after_deduction                         # 給与所得控除後の金額
       model.withholding_tax = get_withholding_tax                         # 源泉徴収税額
       model.social_insurance = get_social_insurance                       # 社会保険料等の金額
       model.life_insurance_deduction = get_life_insurance_deduction       # 住宅借入金等特別控除の額
+      model.exemption_for_dependents = e.dependents                       #扶養控除額
+      model.special_exemption_for_spouse = e.special_tax_for_spouse       #配偶者特別控除額
+      model.exemption_for_spouse = e.spouse                               #配偶者控除額
       model
     end
     
@@ -32,8 +36,14 @@ module Reports
       return logic.get_total_base_salary
     end
     
+    # 控除額
+    def get_exemptions
+      logic = PayrollInfo::PayrollLogic.new(@finder)
+      return logic.get_exemptions
+    end
+    
     # 所得控除の額の合計
-    def get_exemption
+    def get_total_exemption
       logic = PayrollInfo::PayrollLogic.new(@finder)
       return logic.get_exemption
     end

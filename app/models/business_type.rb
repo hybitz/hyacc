9a7aee8baf776b5f53eccee4a69c741ebbdd84e4
ichy <ichylinux@gmail.com>
2014-12-12 16:08:45 +0900

    リファクタリング

--- app/models/business_type.rb
+++ app/models/business_type.rb
@@ -1,12 +1,5 @@
-# -*- encoding : utf-8 -*-
-#
-# $Id: business_type.rb 2768 2011-12-29 17:00:06Z ichy $
-# Product: hyacc
-# Copyright 2009-2011 by Hybitz.co.ltd
-# ALL Rights Reserved.
-#
 class BusinessType < ActiveRecord::Base
-  
+
   def deemed_tax_percent
     deemed_tax_ratio * 100
   end

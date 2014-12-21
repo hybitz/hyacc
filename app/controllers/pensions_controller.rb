5ebee6da31c0ea6b5b1119b00bccc49fff6d1cfd
ichy <ichylinux@gmail.com>
2014-12-21 12:52:00 +0900

    リファクタリング

--- app/controllers/pensions_controller.rb
+++ app/controllers/pensions_controller.rb
@@ -1,4 +1,20 @@
-class PensionsController < Base::HyaccMasterController
-  view_attribute :title => '厚生年金保険料'
-  view_attribute :finder, :class => PensionFinder, :only => :index
+class PensionsController < Base::HyaccController
+  view_attribute :title => '健康保険料'
+  helper_method :finder
+
+  def index
+    @list = finder.list if params[:commit]
+  end
+
+  private
+
+  def finder
+    unless @finder
+      @finder = PensionFinder.new(params[:pension_finder])
+      @finder.ym = Date.today.strftime("%Y-%m") unless @finder.ym.present?
+    end
+
+    @finder
+  end
+
 end

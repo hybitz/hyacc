7817ed7f33ca7d9e929a923c57fba2bc62ab4caa
ichy <ichylinux@gmail.com>
2015-02-01 13:25:48 +0900

    リファクタリング

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,13 +1,10 @@
 class Bank < ActiveRecord::Base
   include HyaccConstants
-  LABELS = {:name => '金融機関名称', :code => '金融機関コード', :deleted => '状態'}
 
   validates :code, :presence => true
+  validates :name, :presence => true
 
   has_many :bank_offices
   accepts_nested_attributes_for :bank_offices
 
-  def self.label(col)
-    LABELS[col]
-  end
 end

d4a4155722af6d83e3477fb01d4009928d7494b3
hiro <hiroyuki@hybitz.co.jp>
2015-03-17 10:42:08 +0900

    テスト追加

--- test/models/payroll_info/payroll_logic_test.rb
+++ test/models/payroll_info/payroll_logic_test.rb
@@ -1,48 +1,76 @@
 require 'test_helper'
 
 class PayrollInfo::PayrollLogicTest < ActiveSupport::TestCase
-
-  setup do
+  
+  def logic_builder(calendar_year)
     finder = PayrollFinder.new(users(:first))
-    finder.calendar_year = 2008
+    finder.calendar_year = calendar_year
     finder.employee_id = user.employee.id
-    @logic = PayrollInfo::PayrollLogic.new(finder)
+    return PayrollInfo::PayrollLogic.new(finder)
   end
   
   def test_get_total_base_salary
-    assert_equal 2653000, @logic.get_total_base_salary
+    logic = logic_builder(2008)
+    assert_equal 2_653_000, logic.get_total_base_salary
   end
   
   def test_get_base_salarys
-    assert_equal 349000, @logic.get_base_salarys["20080507"]
+    logic = logic_builder(2008)
+    assert_equal 349_000, logic.get_base_salarys["20080507"]
   end
   
   def test_get_base_bonuses
-    finder = PayrollFinder.new(user)
-    finder.calendar_year = 2012
-    pLogic = PayrollInfo::PayrollLogic.new(finder)
-    assert_equal 800000, pLogic.get_base_bonuses["20120120"]
+    logic = logic_builder(2012)
+    assert_equal 800_000, logic.get_base_bonuses["20120120"]
   end
 
   def test_get_deduction
-    assert_equal 975600, @logic.get_deduction
+    logic = logic_builder(2008)
+    assert_equal 975_600, logic.get_deduction
   end
     
   def test_get_after_deduction
+    logic = logic_builder(2008)
     # みなし給与のため給与を4000で除して端数を削除後に4000を掛ける
-    assert_equal 2652000 - 975600, @logic.get_after_deduction
+    assert_equal 2_652_000 - 975_600, logic.get_after_deduction
   end
     
   def test_get_exemption
-    assert_equal 1074955, @logic.get_exemption
+    logic = logic_builder(2008)
+    assert_equal 1_074_955, logic.get_exemption
   end
     
   def test_get_withholding_tax
+    logic = logic_builder(2008)
     # (2652000 - 975600 - 1074955)/1000 * 1000 * 0.05 /100 *100
-    assert_equal 30000, @logic.get_withholding_tax
+    assert_equal 30_000, logic.get_withholding_tax
+  end
+  
+  def test_get_health_insurance
+    logic = logic_builder(2012)
+    assert_equal 230_184, logic.get_health_insurance
+  end
+  
+  def test_get_employee_pention
+    logic = logic_builder(2012)
+    assert_equal 381_033, logic.get_employee_pention
+  end
+  
+  def test_get_withholding_taxes_salary
+    logic = logic_builder(2012)
+    withholding_taxes = logic.get_withholding_taxes_salary
+    assert_equal 18_610, withholding_taxes["20120106"]
+  end
+  
+  def test_get_withholding_taxes_of_bonus
+    logic = logic_builder(2012)
+    withholding_taxes = logic.get_withholding_taxes_of_bonus
+    assert_equal 55675, withholding_taxes["20120120"]
   end
   
   def test_get_withholding_taxes
-    assert_equal 8970, @logic.get_withholding_taxes["20080507"]
+    logic = logic_builder(2012)
+    withholding_taxes = logic.get_withholding_taxes(false)
+    assert_equal 18610, withholding_taxes["20120106"]
   end
 end

ad4239ddc4199a2017b33366b518ee6b778a99d6
hiro <hiroyuki@hybitz.co.jp>
2015-04-07 22:19:53 +0900

    テスト追加

---
+++ test/models/reports/withholding_calc_logic_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class Reports::WithholdingCalcLogicTest < ActiveSupport::TestCase
+
+  def test_get_annual_tax
+    finder = WithholdingSlipFinder.new(users(:first))
+    finder.calendar_year = 2012
+    finder.company_id = Company.first.id
+    logic = Reports::WithholdingCalcLogic.new(finder)
+    assert_equal 126300, logic.get_annual_tax
+  end
+
+end

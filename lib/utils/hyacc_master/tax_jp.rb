24357f7ca87c3e950925313c20f6dd5f75d4e4b3
ichy <ichylinux@gmail.com>
2015-07-31 12:01:21 +0900

    refactor

--- lib/utils/hyacc_master/tax_jp.rb
+++ lib/utils/hyacc_master/tax_jp.rb
@@ -5,34 +5,23 @@ class HyaccMaster::TaxJp
 
     insurances = []
     TaxJp::SocialInsurance.find_all_by_date_and_prefecture(date, prefecture_code).each do |si|
-      insurances << convert_tax_jp(si, :as => 'insurance')
+      insurances << convert_tax_jp(si)
     end
     insurances
-  end
-
-  def get_pensions(prefecture_code, ym, base_salary)
-    date = Date.strptime("#{ym}01", '%Y%m%d')
-
-    pensions = []
-    TaxJp::SocialInsurance.find_all_by_date_and_prefecture(date, prefecture_code).each do |si|
-      next unless si.grade.pension_grade >= 1 and si.grade.pension_grade <= 30
-      pensions << convert_tax_jp(si, :as => 'pension')
-    end
-    pensions
   end
 
   def get_pension(ym, prefecture_code, base_salary)
     date = Date.strptime("#{ym}01", '%Y%m%d')
     si = TaxJp::SocialInsurance.find_by_date_and_prefecture_and_salary(date, prefecture_code, base_salary)
 
-    pension = convert_tax_jp(si, :as => 'pension')
+    pension = convert_tax_jp(si)
   end
 
   def get_insurance(ym, prefecture_code, base_salary)
     date = Date.strptime("#{ym}01", '%Y%m%d')
     si = TaxJp::SocialInsurance.find_by_date_and_prefecture_and_salary(date, prefecture_code, base_salary)
 
-    insurance = convert_tax_jp(si, :as => 'insurance')
+    insurance = convert_tax_jp(si)
   end
 
   #標準報酬の基本情報を取得
@@ -42,35 +31,28 @@ class HyaccMaster::TaxJp
     date = Date.strptime("#{ym}01", '%Y%m%d')
     si = TaxJp::SocialInsurance.find_by_date_and_prefecture_and_salary(date, prefecture_code, base_salary)
 
-    insurance = convert_tax_jp(si, :as => 'grade')
+    insurance = convert_tax_jp(si)
   end
 
   private
 
-  def convert_tax_jp(si, options = {})
+  def convert_tax_jp(si)
     ret = Insurance.new
     ret.apply_start_ym = si.valid_from
     ret.apply_end_ym = si.valid_until
 
-    case options[:as]
-    when 'grade'
-      ret.grade = si.grade.grade
-      ret.pension_grade = si.grade.pension_grade
-    when 'insurance'
-      ret.grade = si.grade.grade
-      ret.pension_grade = si.grade.pension_grade
-    when 'pension'
-      ret.grade = si.grade.pension_grade
-    end
-
+    ret.grade = si.grade.grade
+    ret.pension_grade = si.grade.pension_grade
     ret.pay_range_above = si.grade.salary_from
     ret.pay_range_under = si.grade.salary_to
     ret.monthly_earnings = si.grade.monthly_standard
     ret.daily_earnings = si.grade.daily_standard
+
     ret.health_insurance_all = si.health_insurance.general_amount
     ret.health_insurance_half = si.health_insurance.general_amount_half
     ret.health_insurance_all_care = si.health_insurance.general_amount_care
     ret.health_insurance_half_care = si.health_insurance.general_amount_care_half
+
     ret.welfare_pension_insurance_all = si.welfare_pension.general_amount
     ret.welfare_pension_insurance_half = si.welfare_pension.general_amount_half
     ret.welfare_pension2_insurance_all = si.welfare_pension.particular_amount

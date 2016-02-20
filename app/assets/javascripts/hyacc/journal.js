dcce58b587fb1a806517a0ad0830c90462f002c2
ichy <ichylinux@gmail.com>
2016-02-20 17:55:00 +0900

    refactor

--- app/assets/javascripts/hyacc/journal.js
+++ app/assets/javascripts/hyacc/journal.js
@@ -3,39 +3,25 @@ hyacc.Journal = function(options) {
   this._init();
 };
 
-// 全明細の自動振替の単一選択チェック
-hyacc.Journal.prototype.check_auto_journal_types = function() {
-  var errors = [];
+hyacc.Journal.prototype.get_details = function() {
+  var ret = [];
 
-  var that = this;
-  this._get_details().each(function(i) {
-    var tr = $(this);
-    var count = 0;
-
-    for (var j = 0; j < 3; j ++) {
-      var tr = $(tr).next();
-      var checked = $(tr).find('input[name*="\\[auto_journal_type\\]"]').prop('checked');
-      if (checked) {
-        count ++;
-      }
-    }
-
-    if (count > 1) {
-      errors.push('【明細' + (i+1) + '】自動振替は複数指定できません。');
-      that.show_detail(this);
-    }
+  this._get_details().each(function() {
+    ret.push($(this).get(0));
   });
+  
+  return ret;
+};
 
-  if (errors.length > 0) {
-    alert(errors.join('\n'));
-    return false;
+hyacc.Journal.prototype.show_detail = function(tr) {
+  for (var i = 0; i < 3; i ++) {
+    var tr = $(tr).next();
+    tr.show();
   }
-
-  return true;
 };
 
 // 自動振替時の日付必須チェック
-hyacc.Journal.prototype.check_auto_journal_dates = function() {
+hyacc.Journal.prototype._check_auto_journal_dates = function() {
   var errors = [];
 
   var that = this;
@@ -60,52 +46,31 @@ hyacc.Journal.prototype.check_auto_journal_dates = function() {
   return true;
 };
 
-hyacc.Journal.prototype.get_details = function() {
-  var ret = [];
-
-  this._get_details().each(function() {
-    ret.push($(this).get(0));
-  });
-  
-  return ret;
-};
-
-hyacc.Journal.prototype.show_detail = function(tr) {
-  for (var i = 0; i < 3; i ++) {
-    var tr = $(tr).next();
-    tr.show();
-  }
-};
-
-hyacc.Journal.prototype.validate_amount = function() {
+// 全明細の自動振替の単一選択チェック
+hyacc.Journal.prototype._check_auto_journal_types = function() {
   var errors = [];
 
+  var that = this;
   this._get_details().each(function(i) {
-    var amount = parseInt($(this).find('input[name*="\\[input_amount\\]"]').val());
-    if (isNaN(amount) || amount == 0) {
-      errors.push((i + 1) + '行目の金額が未入力です。');
+    var tr = $(this);
+    var count = 0;
+
+    for (var j = 0; j < 3; j ++) {
+      var tr = $(tr).next();
+      var checked = $(tr).find('input[name*="\\[auto_journal_type\\]"]').prop('checked');
+      if (checked) {
+        count ++;
+      }
+    }
+
+    if (count > 1) {
+      errors.push('【明細' + (i+1) + '】自動振替は複数指定できません。');
+      that.show_detail(this);
     }
   });
 
   if (errors.length > 0) {
     alert(errors.join('\n'));
-    return false;
-  }
-
-  return true;
-};
-  
-hyacc.Journal.prototype.validate_amount_balance = function() {
-  var debit = $('#journal_debit_amount_sum').text();
-  var credit = $('#journal_credit_amount_sum').text();
-  
-  if (debit != credit) {
-    alert('貸借の金額が一致しません。');
-    return false;
-  }
-
-  if (debit == 0 || credit == 0) {
-    alert('金額が０円です。');
     return false;
   }
 
@@ -147,6 +112,7 @@ hyacc.Journal.prototype._get_details = function() {
 
 hyacc.Journal.prototype._init = function() {
   this._init_shortcut();
+  this._init_validation();
 };
 
 hyacc.Journal.prototype._init_shortcut = function() {
@@ -166,4 +132,66 @@ hyacc.Journal.prototype._init_shortcut = function() {
       }, {target: $(this).get(0)}
     );
   });
+};
+
+hyacc.Journal.prototype._init_validation = function() {
+  var that = this;
+  $(this.selector).submit(function() {
+    return that._validate_journal();
+  });
+};
+
+hyacc.Journal.prototype._validate_amount = function() {
+  var errors = [];
+
+  this._get_details().each(function(i) {
+    var amount = parseInt($(this).find('input[name*="\\[input_amount\\]"]').val());
+    if (isNaN(amount) || amount == 0) {
+      errors.push((i + 1) + '行目の金額が未入力です。');
+    }
+  });
+
+  if (errors.length > 0) {
+    alert(errors.join('\n'));
+    return false;
+  }
+
+  return true;
+};
+  
+hyacc.Journal.prototype._validate_amount_balance = function() {
+  var debit = $('#journal_debit_amount_sum').text();
+  var credit = $('#journal_credit_amount_sum').text();
+  
+  if (debit != credit) {
+    alert('貸借の金額が一致しません。');
+    return false;
+  }
+
+  if (debit == 0 || credit == 0) {
+    alert('金額が０円です。');
+    return false;
+  }
+
+  return true;
+};
+
+hyacc.Journal.prototype._validate_journal = function() {
+  if (! this._check_auto_journal_types()) {
+    return false;
+  }
+  
+  if (! this._check_auto_journal_dates()) {
+    return false;
+  }
+
+  if (! this._validate_amount()) {
+      return false;
+  }
+
+  if (! this._validate_amount_balance()) {
+      return false;
+  }
+  
+  return true;
 };

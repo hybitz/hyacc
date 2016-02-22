6120312695d508fa7a029ed21ecddcae94737be0
ichy <ichylinux@gmail.com>
2016-02-22 23:17:11 +0900

    refs #244

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151220055136) do
+ActiveRecord::Schema.define(version: 20160222140404) do
 
   create_table "accounts", force: true do |t|
     t.string   "code",                                   default: "",    null: false

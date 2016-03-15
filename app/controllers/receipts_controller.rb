5b60d08b7bf204c9bdc129fc15e0cc66f9124e6d
ichy <ichylinux@gmail.com>
2016-03-16 08:00:34 +0900

    refactor

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -2,7 +2,9 @@ class ReceiptsController < ApplicationController
 
   def show
     @receipt = Receipt.find(params[:id])
-    send_file @receipt.file.path, :filename => @receipt.original_filename
+    send_file @receipt.file.path,
+        :filename => @receipt.original_filename,
+        :type => MimeMagic.by_path(@receipt.original_filename)
   end
 
 end

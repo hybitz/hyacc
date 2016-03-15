d23efefe55ff7502c7330af15db62cbc6eb7fcfa
ichy <ichylinux@gmail.com>
2016-03-16 07:05:03 +0900

    refactor

--- db/migrate/20160314225231_update_receipts.rb
+++ db/migrate/20160314225231_update_receipts.rb
@@ -1,8 +1,9 @@
 class UpdateReceipts < ActiveRecord::Migration
   def up
     Journal.where('receipt_path is not null').find_each(:batch_size => 100) do |j|
-      next if j.receipt_path.blank?
       next if j.receipt.present?
+      next if j.receipt_path.blank?
+      next unless File.exist?(j.receipt_path)
 
       puts "#{j.id}: #{j.receipt_path}"
       r = j.build_receipt(:file => File.new(File.join(UPLOAD_DIRECTORY, j.receipt_path)))

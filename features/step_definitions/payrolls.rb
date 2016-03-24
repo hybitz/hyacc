502374c9564599ff91c4de78ddecd3a1306896b0
ichy <ichylinux@gmail.com>
2016-03-25 00:30:22 +0900

    refactor

--- features/step_definitions/payrolls.rb
+++ features/step_definitions/payrolls.rb
@@ -1,8 +1,8 @@
 ならば /^賃金台帳の一覧に遷移する$/ do
   assert has_title?('賃金台帳')
-  assert_url '/payrolls$'
+  assert_url '/payrolls(\?.*)?'
 end
 
 ならば /^賃金台帳が表示される$/ do
-  assert_url '/payrolls$'
+  assert_url '/payrolls(\?.*)?'
 end
--- features/step_definitions/report.rb
+++ features/step_definitions/report.rb
@@ -1,15 +1,15 @@
 ならば /^帳票出力に遷移する$/ do
   assert has_title?('帳票出力')
-  assert_url '/report$'
+  assert_url '/report(\?.*)?'
 end
 
 ならば /^源泉徴収に遷移する$/ do
   assert has_title?('源泉徴収')
-  assert_url '/withholding_slip$'
+  assert_url '/withholding_slip(\?.*)?'
 end
 
 ならば /^給与所得の源泉徴収等の法定調書合計表の一覧に遷移する$/ do
-  assert_url '/withholding_slip$'
+  assert_url '/withholding_slip(\?.*)?'
 end
 
 かつ /^メッセージ「(.*?)」が表示(される|されない)$/ do |message, result|
@@ -36,7 +36,7 @@ end
       fail "想定外の項目 #{row[0]} です。"
     end
   end
-  
+
   form_selector = "#finders"
   within form_selector do
     select '2009', :from => 'finder_calendar_year'

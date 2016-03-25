5bf95e41793e34e5921537d5838878721a1ab9e8
ichy <ichylinux@gmail.com>
2016-03-26 02:44:05 +0900

    refactor

--- features/support/dialog_support.rb
+++ features/support/dialog_support.rb
@@ -3,9 +3,12 @@ module DialogSupport
   def has_dialog?(options = {})
     options = {:title => options} if options.is_a?(String) or options.is_a?(Regexp)
 
+    assert has_selector?('.ui-dialog')
     if options[:title]
       has_selector?("span.ui-dialog-title", :text => options[:title])
     end
+
+    true
   end
 
   def has_no_dialog?(options = {})
@@ -14,6 +17,9 @@ module DialogSupport
     if options[:title]
       has_no_selector?("span.ui-dialog-title", :text => options[:title])
     end
+    assert has_no_selector?('.ui-dialog')
+
+    true
   end
 
 end

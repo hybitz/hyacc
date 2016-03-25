d251b925e299b6f5262324f8dc21142dd8303d06
ichy <ichylinux@gmail.com>
2016-03-26 01:36:07 +0900

    cucumber-rails

--- user_stories/support/env.rb
+++ user_stories/support/env.rb
@@ -1,11 +1,13 @@
 require 'closer'
-require 'rails'
-require 'daddy/cucumber/helpers'
-require 'capybara/cucumber'
-require 'capybara/poltergeist' if ENV['DRIVER'] == 'poltergeist'
 
-Capybara.default_driver = (ENV['DRIVER'] || :selenium).to_sym
-Capybara.default_selector = :css
+begin
+  require 'cucumber/rails'
+  Cucumber::Rails::Database.autorun_database_cleaner = false
+rescue LoadError => e
+  puts 'cucumber-rails not found.'
+end
+
+require 'daddy/cucumber'
 
 include HyaccConstants
 
@@ -13,21 +15,21 @@ if ENV['CI'] != 'travis'
   Before do |scenario|
     feature_file = scenario.feature.location.file
     dump_dir = File.join('tmp', File.dirname(feature_file), File.basename(feature_file, '.feature'))
-  
+
     if ARGV.include?(feature_file)
       db = Dir.glob(File.join(dump_dir, '*.dump.gz')).first
       if db
-        system("rake dad:db:load DUMP_FILE=#{db} --quiet")
+        system("bundle exec rake dad:db:load DUMP_FILE=#{db} --quiet")
       end
     else
       # 直前のDBをダンプしておく
       if @current_feature != feature_file
         system("rm -Rf #{dump_dir}")
-        system("rake dad:db:dump DUMP_DIR=#{dump_dir} --quiet")
+        system("bundle exec rake dad:db:dump DUMP_DIR=#{dump_dir} --quiet")
         @current_feature = feature_file
       end
     end
-  
+
     resize_window(1280, 720)
   end
 end

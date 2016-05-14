c39033a09ed2ccc784ecef44385e359e638945a9
ichy <ichylinux@gmail.com>
2016-05-14 11:00:33 +0900

    refactor

--- app/models/auto/journal/payroll_param.rb
+++ app/models/auto/journal/payroll_param.rb
@@ -1,10 +1,3 @@
-# -*- encoding : utf-8 -*-
-#
-# $Id: payroll_param.rb 2474 2011-03-23 15:28:08Z ichy $
-# Product: hyacc
-# Copyright 2009 by Hybitz.co.ltd
-# ALL Rights Reserved.
-#
 module Auto::Journal
     
   class PayrollParam < Auto::AutoJournalParam

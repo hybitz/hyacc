7f97af737bda9a3c5e9a0929d262fa12c8ba2701
ichy <ichylinux@gmail.com>
2016-05-20 08:23:52 +0900

    refactor

--- app/assets/javascripts/hyacc/simple_slip_settings.js
+++ app/assets/javascripts/hyacc/simple_slip_settings.js
@@ -3,11 +3,11 @@ var simple_slip_settings = {};
 simple_slip_settings.add_simple_slip_setting = function(trigger) {
   var url = $(trigger).attr('href');
   var params = {
-    index: $(trigger).closest('table').find('tr').length,
+    index: $(trigger).closest('table').find('tbody').find('tr').length,
     format: 'html'
   };
 
   $.get(url, params, function(html) {
-    $(trigger).closest('tr').before(html);
+    $(trigger).closest('table').find('tbody').append(html);
   });
 };

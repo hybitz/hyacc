d5a614f263422f9c4f0f5c2967c38add54232bf8
ichy <ichylinux@gmail.com>
2016-06-07 01:12:40 +0900

    user_stories

--- user_stories/step_definitions/branches.rb
+++ user_stories/step_definitions/branches.rb
@@ -1,14 +1,16 @@
-もし /^配属先となる(.*?)を登録$/ do |branch_name|
+もし /^部門 (.*?) を登録$/ do |branch_name|
   visit_branches
   click_on '本店'
   within_dialog('部門　参照') do
     click_on '子部門追加'
   end
 
+  code = (Branch.where(:company_id => current_user.company_id).maximum(:code).to_i + 100).to_s
+
   within_dialog('部門　追加') do
     with_capture do
       select '本社', :from => '事業所'
-      fill_in '部門コード', :with => '200'
+      fill_in '部門コード', :with => code
       fill_in '部門名', :with => branch_name
     end
     click_on '登録'

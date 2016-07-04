80e1f316b1d680312f8218fd14898c1ae8958f64
ichy <ichylinux@gmail.com>
2016-07-04 22:13:48 +0900

    役員給与の補助科目に従業員を設定

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160628151647) do
+ActiveRecord::Schema.define(version: 20160704130658) do
 
   create_table "accounts", force: true do |t|
     t.string   "code",                                   default: "",    null: false

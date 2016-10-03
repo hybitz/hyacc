ac7136e64f26dcf521d6e134047ec6e7c541b822
ichy <ichylinux@gmail.com>
2016-10-03 22:42:03 +0900

    refactor

--- app/models/sub_account.rb
+++ app/models/sub_account.rb
@@ -1,10 +1,3 @@
-# coding: UTF-8
-#
-# $Id: sub_account.rb 3310 2014-01-27 13:20:06Z ichy $
-# Product: hyacc
-# Copyright 2009-2014 by Hybitz.co.ltd
-# ALL Rights Reserved.
-#
 class SubAccount < ActiveRecord::Base
   belongs_to :account
 

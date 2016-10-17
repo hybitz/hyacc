b3f8021bef2041bf98154b526ac6fff3df973e52
ichy <ichylinux@gmail.com>
2016-10-17 23:07:33 +0900

    デフォルトは本社の都道府県

--- app/controllers/mv/social_insurances_controller.rb
+++ app/controllers/mv/social_insurances_controller.rb
@@ -11,7 +11,8 @@ class Mv::SocialInsurancesController < Base::HyaccController
   def finder
     unless @finder
       @finder = SocialInsuranceFinder.new(params[:social_insurance_finder])
-      @finder.ym = Date.today.strftime("%Y-%m") unless @finder.ym.present?
+      @finder.ym ||= Date.today.strftime("%Y-%m")
+      @finder.prefecture_code ||= current_company.head_branch.business_office.prefecture_code
     end
 
     @finder

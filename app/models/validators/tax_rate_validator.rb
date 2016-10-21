eedad8a3dcf69b1d80bbe0bf64b49364de9cddb2
ichy <ichylinux@gmail.com>
2016-10-21 12:36:34 +0900

    消費税率のバリデーションを修正

--- app/models/validators/tax_rate_validator.rb
+++ app/models/validators/tax_rate_validator.rb
@@ -3,14 +3,10 @@ module Validators
   class TaxRateValidator < ActiveModel::Validator
 
     def validate(record)
-      if record.tax_type_nontaxable?
-        if record.tax_rate.to_f != 0
-          record.errors[:tax_rate] << I18n.t('errors.messages.invalid')
-        end
-      else
-        if record.tax_rate.to_f == 0
-          record.errors[:tax_rate] << I18n.t('errors.messages.blank')
-        end
+      return if record.tax_type_nontaxable?
+
+      if record.tax_rate.to_f == 0
+        record.errors[:tax_rate] << I18n.t('errors.messages.blank')
       end
     end
 

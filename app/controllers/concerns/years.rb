2f4222389074b4be627a81a32412bb5975e36f2c
ichy <ichylinux@gmail.com>
2016-12-31 18:16:32 +0900

    refactor

--- app/controllers/concerns/years.rb
+++ app/controllers/concerns/years.rb
@@ -3,10 +3,22 @@ module Years
 
   included do
     helper_method :calendar_years
+    helper_method :fiscal_years
   end
 
   private
 
+  # 会計年度選択リストを取得する
+  def fiscal_years
+    if @_fiscal_years.nil?
+      first = current_user.company.founded_fiscal_year.fiscal_year
+      last = current_user.company.fiscal_years.last.fiscal_year
+      @_fiscal_years = HyaccDateUtil.get_ym_list( first, 0, last - first )
+    end
+
+    @_fiscal_years
+  end
+  
   # 暦年選択リストを取得する
   def calendar_years
     if @_calendar_years.nil?

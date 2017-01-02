1409271d2fed2d60f0da3dcdce5756eb15254862
ichy <ichylinux@gmail.com>
2017-01-02 22:59:15 +0900

    bundle update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.7.1'
+lock '3.6.1'
 
 set :application, 'hyacc'
 set :repo_url, `git config --get remote.origin.url`
@@ -9,25 +9,30 @@ set :repo_url, `git config --get remote.origin.url`
 set :branch, ENV['BRANCH'] || 'release'
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+# set :deploy_to, '/var/www/my_app_name'
 set :deploy_to, "/var/apps/#{fetch(:application)}"
+
+# Default value for :scm is :git
+# set :scm, :git
+set :scm, :git
+set :git_strategy, MyGitStrategy
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
 # You can configure the Airbrussh format using :format_options.
 # These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
 
 # Default value for :pty is false
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
 append :linked_files, 'config/secrets.yml'
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 # Default value for default_env is {}

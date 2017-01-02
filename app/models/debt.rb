11a63f4d52aace274b545eddeb0d228114346bd0
ichy <ichylinux@gmail.com>
2017-01-02 20:52:33 +0900

    refactor

--- app/models/debt.rb
+++ app/models/debt.rb
@@ -1,10 +1,3 @@
-# -*- encoding : utf-8 -*-
-#
-# $Id: debt.rb 2471 2011-03-23 14:59:36Z ichy $
-# Product: hyacc
-# Copyright 2009 by Hybitz.co.ltd
-# ALL Rights Reserved.
-#
 class Debt
   # 仮負債フィールド
   attr_accessor :id

22c434d95f3c94536f3d45fee60937ace6f393cb
ichy <ichylinux@gmail.com>
2017-01-02 22:34:25 +0900

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,30 +9,25 @@ set :repo_url, `git config --get remote.origin.url`
 set :branch, ENV['BRANCH'] || 'release'
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+# set :deploy_to, "/var/www/my_app_name"
 set :deploy_to, "/var/apps/#{fetch(:application)}"
-
-# Default value for :scm is :git
-# set :scm, :git
-set :scm, :git
-set :git_strategy, MyGitStrategy
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
 # You can configure the Airbrussh format using :format_options.
 # These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+# append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_files, 'config/secrets.yml'
 
 # Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 # Default value for default_env is {}

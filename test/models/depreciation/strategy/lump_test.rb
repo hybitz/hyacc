a5a554697b5b4fec8806bb7abef8859a5c771584
ichy <ichylinux@gmail.com>
2017-02-25 10:09:39 +0900

    refactor

--- test/models/depreciation/strategy/lump_test.rb
+++ test/models/depreciation/strategy/lump_test.rb
@@ -1,36 +1,34 @@
 require 'test_helper'
 
-module Depreciation::Strategy
-  class LumpTest < ActiveSupport::TestCase
+class Depreciation::Strategy::LumpTest < ActiveSupport::TestCase
 
-    def test_create_depreciations
-      asset = Asset.find(1)
-      asset.depreciation_method = DEPRECIATION_METHOD_LUMP
-      asset.depreciation_limit = 0
-      Depreciation::DepreciationUtil.create_depreciations(asset)
-      assert_nothing_raised{ asset.save! }
-      
-      assert_equal 2009, asset.start_fiscal_year
-      assert_equal 2009, asset.end_fiscal_year
-      assert_equal 1, asset.depreciations.size
-      assert_equal 100000, asset.depreciations[0].amount_at_start
-      assert_equal 0, asset.depreciations[0].amount_at_end
-    end
-
-    def test_年度途中で取得した資産
-      asset = Asset.find(1)
-      asset.depreciation_method = DEPRECIATION_METHOD_LUMP
-      asset.depreciation_limit = 0
-      asset.ym = 200906
-      Depreciation::DepreciationUtil.create_depreciations(asset)
-      assert asset.save
-      
-      assert_equal 2009, asset.start_fiscal_year
-      assert_equal 2009, asset.end_fiscal_year
-      assert_equal 1, asset.depreciations.size
-      assert_equal 100000, asset.depreciations[0].amount_at_start
-      assert_equal 0, asset.depreciations[0].amount_at_end
-    end
+  def test_create_depreciations
+    asset = Asset.find(1)
+    asset.depreciation_method = DEPRECIATION_METHOD_LUMP
+    asset.depreciation_limit = 0
+    Depreciation::DepreciationUtil.create_depreciations(asset)
+    assert asset.save
     
+    assert_equal 2009, asset.start_fiscal_year
+    assert_equal 2009, asset.end_fiscal_year
+    assert_equal 1, asset.depreciations.size
+    assert_equal 100000, asset.depreciations[0].amount_at_start
+    assert_equal 0, asset.depreciations[0].amount_at_end
   end
+
+  def test_年度途中で取得した資産
+    asset = Asset.find(1)
+    asset.depreciation_method = DEPRECIATION_METHOD_LUMP
+    asset.depreciation_limit = 0
+    asset.ym = 200906
+    Depreciation::DepreciationUtil.create_depreciations(asset)
+    assert asset.save
+    
+    assert_equal 2009, asset.start_fiscal_year
+    assert_equal 2009, asset.end_fiscal_year
+    assert_equal 1, asset.depreciations.size
+    assert_equal 100000, asset.depreciations[0].amount_at_start
+    assert_equal 0, asset.depreciations[0].amount_at_end
+  end
+    
 end

bac857ef07ba578f640bad5931e8ccda14241b94
ichy <ichylinux@gmail.com>
2017-03-17 17:16:01 +0900

    test

--- test/controllers/housework_details_controller_test.rb
+++ test/controllers/housework_details_controller_test.rb
@@ -4,22 +4,23 @@ class HouseworkDetailsControllerTest < ActionController::TestCase
 
   def test_個人事業主でなければ利用不可
     sign_in user
-    xhr :get, :new, :housework_id => housework.id
+    get :new, :xhr => true, :params => {:housework_id => housework.id}
     assert_response :forbidden
   end
 
   def test_追加
     sign_in freelancer
-    xhr :get, :new, :housework_id => housework.id
+    get :new, :xhr => true, :params => {:housework_id => housework.id}
     assert_response :success
     assert_template :new
   end
 
   def test_登録
     sign_in freelancer
-    xhr :post, :create,
+    post :create, :xhr => true, :params => {
         :housework_id => housework.id,
         :housework_detail => {:account_id => expense_account.id, :business_ratio => 80}
+    }
     assert_response :success
     assert_template 'common/reload'
   end
--- test/controllers/mm/rents_controller_test.rb
+++ test/controllers/mm/rents_controller_test.rb
@@ -13,29 +13,35 @@ class Mm::RentsControllerTest < ActionController::TestCase
   end
 
   def test_should_get_new
-    xhr :get, :new
+    get :new, :xhr => true
     assert_response :success
     assert_not_nil assigns(:rent)
   end
 
   def test_should_create_rent
     assert_difference 'Rent.count', 1 do
-      xhr :post, :create,
-          :rent => {:rent_type => "1", :usage_type => "1",
-                    :address => "住所", :customer_id => "1",
-                    :name => "表示名",
-                    :status => "1", :ymd_start => "20071010" }
+      post :create, :xhr => true, :params => {
+        :rent => {
+          :rent_type => "1", :usage_type => "1",
+          :address => "住所", :customer_id => "1",
+          :name => "表示名",
+          :status => "1", :ymd_start => "20071010"
+        }
+      }
     end
 
     assert_response :success
   end
   
   def test_should_create_rent_with_errors
-    xhr :post, :create,
-        :rent => {:rent_type => "", :usage_type => "",
-                  :address => "住所", :customer_id => "1",
-                  :name => "",
-                  :status => "", :ymd_start => "", :ymd_end => "2009010" }
+    post :create, :xhr => true, :params => {
+      :rent => {
+        :rent_type => "", :usage_type => "",
+        :address => "住所", :customer_id => "1",
+        :name => "",
+        :status => "", :ymd_start => "", :ymd_end => "2009010"
+      }
+    }
     
     assert_response :success
     assert_equal 6, assigns(:rent).errors.size
@@ -43,23 +49,23 @@ class Mm::RentsControllerTest < ActionController::TestCase
   end
   
   def test_should_get_edit
-    xhr :get, :edit, :id => rents(:rent_00005).id
+    get :edit, :xhr => true, :params => {:id => rents(:rent_00005).id}
     assert_response :success
   end
 
   def test_should_update_rent
-    put :update, :id => rents(:rent_00005).id, :format => 'js',
+    patch :update, :params => {:id => rents(:rent_00005).id, :format => 'js',
                               :rent => {:rent_type => "1", :usage_type => "1",
                                         :address => "住所", :customer_id => "1",
                                         :name => "表示名",
-                                        :status => "0" }
+                              :status => "0" }}
     assert_response :success
     assert_template 'common/reload'
   end
 
   def test_should_destroy_rent
     assert_difference('Rent.count', -1) do
-      delete :destroy, :id => rents(:rent_00005).id
+      delete :destroy, :params => {:id => rents(:rent_00005).id}
     end
 
     assert_redirected_to :action => 'index'

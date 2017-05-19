4f38b91aaf06f6d78aa472eb3ec7c8d87b3cf59f
ichy <ichylinux@gmail.com>
2017-05-19 12:14:16 +0900

    eagar load lib/utils

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module Hyacc
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths += %W( #{Rails.root}/lib/utils )
+    config.autoload_paths += %W( lib/utils )
+    config.paths.add 'lib/utils', eagar_load: true
 
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local

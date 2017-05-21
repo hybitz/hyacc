663da6e2e70bec6ee22f4d97626d3d0dcb205820
ichy <ichylinux@gmail.com>
2017-05-21 12:46:06 +0900

    travis

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,3 +6,18 @@ Dir[File.join(Rails.root, 'test', 'support', '*.rb')].each do |f|
   require f
   include File.basename(f).split('.').first.camelize.constantize
 end
+
+if ENV['CI'] == 'travis'
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+    'tunnel-identifier' => ENV['TRAVIS_JOB_NUMBER']
+  )
+
+  Capybara.default_driver = :selenium
+  Capybara.register_driver :selenium do |app|
+    driver = Capybara::Selenium::Driver.new(app,
+      :browser => :remote,
+      :url => "http://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com/wd/hub",
+      :desired_capabilities => caps
+    )
+  end
+end

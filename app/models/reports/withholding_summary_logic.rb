d67603bd9de737b5d92272f8bd59bc493b61b5c8
hiro <hiro@localhost>
2018-04-20 17:18:06 +0900

    add retirement_date

--- app/models/reports/withholding_summary_logic.rb
+++ app/models/reports/withholding_summary_logic.rb
@@ -13,7 +13,7 @@ module Reports
       model.withholding_tax = get_withholding_tax                         # 源泉徴収税額
       model
     end
-    
+
     # 支払金額
     def get_total_salary
       total_base_salary = 0
@@ -23,20 +23,26 @@ module Reports
         logic = PayrollInfo::PayrollLogic.new(@finder.calendar_year, @finder.employee_id)
         total_base_salary = total_base_salary + logic.get_total_base_salary
       end
-      
+
       total_base_salary
     end
-    
+
     # 源泉徴収税額
     def get_withholding_tax
       total_withholding_tax = 0
       c = Company.find(@finder.company_id)
       c.employees.each do |emp|
         @finder.employee_id = emp.id
+        if emp.employment_date > (@finder.calendar_year + "-12-31").to_date
+          next
+        end
+        if !emp.retirement_date.nil? && emp.retirement_date < (@finder.calendar_year + "-01-01").to_date
+          next
+        end
         logic = PayrollInfo::PayrollLogic.new(@finder.calendar_year, @finder.employee_id)
         total_withholding_tax = total_withholding_tax + logic.get_withholding_tax
       end
-      
+
       total_withholding_tax
     end
   end

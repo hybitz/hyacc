73a765c23da1d30f5de3a416af924e250b1471d4
ichy <ichylinux@gmail.com>
2018-12-05 20:36:48 +0900

    refs #256 リファクタリング

--- app/utils/tax_utils.rb
+++ app/utils/tax_utils.rb
@@ -5,7 +5,7 @@ class TaxUtils
 
     ret = []
     TaxJp::SocialInsurance.find_all_by_date_and_prefecture(date, prefecture_code).each do |si|
-      ret << convert_tax_jp(si)
+      ret << convert_tax_jp_si(si)
     end
     ret
   end
@@ -14,7 +14,19 @@ class TaxUtils
     date = Date.strptime("#{ym}01", '%Y%m%d')
     si = TaxJp::SocialInsurance.find_by_date_and_prefecture_and_salary(date, prefecture_code, base_salary)
 
-    convert_tax_jp(si)
+    convert_tax_jp_si(si)
+  end
+
+  def self.get_health_insurance(ym, prefecture_code)
+    date = Date.strptime("#{ym}01", '%Y%m%d')
+    hi = TaxJp::SocialInsurance.find_health_insurance_by_date_and_prefecture(date, prefecture_code)
+    convert_tax_jp_hi(hi)
+  end
+
+  def self.get_welfare_pension(ym)
+    date = Date.strptime("#{ym}01", '%Y%m%d')
+    wp = TaxJp::SocialInsurance.find_welfare_pension_by_date(date)
+    convert_tax_jp_wp(wp)
   end
 
   # 標準報酬の基本情報を取得
@@ -24,34 +36,8 @@ class TaxUtils
     date = Date.strptime("#{ym}01", '%Y%m%d')
     si = TaxJp::SocialInsurance.find_by_date_and_prefecture_and_salary(date, prefecture_code, base_salary)
 
-    convert_tax_jp(si)
+    convert_tax_jp_si(si)
   end
-
-  def self.convert_tax_jp(si)
-    ret = Insurance.new
-    ret.apply_start_ym = si.valid_from
-    ret.apply_end_ym = si.valid_until
-
-    ret.grade = si.grade.grade
-    ret.pension_grade = si.grade.pension_grade
-    ret.pay_range_above = si.grade.salary_from
-    ret.pay_range_under = si.grade.salary_to
-    ret.monthly_standard = si.grade.monthly_standard
-    ret.daily_earnings = si.grade.daily_standard
-
-    ret.health_insurance_all = si.health_insurance.general_amount
-    ret.health_insurance_half = si.health_insurance.general_amount_half
-    ret.health_insurance_all_care = si.health_insurance.general_amount_care
-    ret.health_insurance_half_care = si.health_insurance.general_amount_care_half
-
-    ret.welfare_pension_insurance_all = si.welfare_pension.general_amount
-    ret.welfare_pension_insurance_half = si.welfare_pension.general_amount_half
-    ret.welfare_pension2_insurance_all = si.welfare_pension.particular_amount
-    ret.welfare_pension2_insurance_half = si.welfare_pension.particular_amount_half
-
-    ret
-  end
-  private_class_method :convert_tax_jp
 
   def self.get_employment_insurance(ym)
     # 対象月の末日を基準
@@ -59,5 +45,44 @@ class TaxUtils
 
     TaxJp::LaborInsurances::EmploymentInsurance.find_by_date(date)
   end
+
+  def self.convert_tax_jp_si(si)
+    ret = Insurance.new
+    ret.valid_from = si.valid_from
+    ret.valid_until = si.valid_until
+
+    ret.grade = si.grade.grade
+    ret.pension_grade = si.grade.pension_grade
+    ret.salary_from = si.grade.salary_from
+    ret.salary_to = si.grade.salary_to
+    ret.monthly_standard = si.grade.monthly_standard
+    ret.daily_standard = si.grade.daily_standard
+    
+    convert_tax_jp_hi(si.health_insurance, ret)
+    convert_tax_jp_wp(si.welfare_pension, ret)
+
+    ret
+  end
+  private_class_method :convert_tax_jp_si
+
+  def self.convert_tax_jp_hi(hi, to = nil)
+    to ||= Insurance.new
+    to.health_insurance_all = hi.general_amount
+    to.health_insurance_half = hi.general_amount_half
+    to.health_insurance_all_care = hi.general_amount_care
+    to.health_insurance_half_care = hi.general_amount_care_half
+    to
+  end
+  private_class_method :convert_tax_jp_hi
+
+  def self.convert_tax_jp_wp(wp, to = nil)
+    to ||= Insurance.new
+    to.welfare_pension_insurance_all = wp.general_amount
+    to.welfare_pension_insurance_half = wp.general_amount_half
+    to.welfare_pension2_insurance_all = wp.particular_amount
+    to.welfare_pension2_insurance_half = wp.particular_amount_half
+    to
+  end
+  private_class_method :convert_tax_jp_wp
 
 end

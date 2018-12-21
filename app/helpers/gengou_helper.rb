11e9037f32c6b6c99f1eaed81fef04cff12ff2b5
hiro <hiroyuki.suzuki@hybitz.co.jp>
2018-12-21 15:11:28 +0900

    和暦表示対応

--- app/helpers/gengou_helper.rb
+++ app/helpers/gengou_helper.rb
@@ -8,4 +8,19 @@ module GengouHelper
     TaxJp::Gengou.to_seireki(gengou, wareki) 
   end
 
+  def heisei?(year)
+    TaxJp::Gengou.to_wareki(year.to_i).start_with?("平成")
+  end
+
+  def syowa?(year)
+    TaxJp::Gengou.to_wareki(year.to_i).start_with?("昭和")
+  end
+  
+  def taisyo?(year)
+    TaxJp::Gengou.to_wareki(year.to_i).start_with?("大正")
+  end
+  
+  def meiji?(year)
+    TaxJp::Gengou.to_wareki(year.to_i).start_with?("明治")
+  end
 end

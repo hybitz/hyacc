a184f498239dc35ec3c604509033ffcea30a20b5
ichy <ichylinux@gmail.com>
2019-01-03 20:24:00 +0900

    refactor

--- app/helpers/financial_statement_helper.rb
+++ app/helpers/financial_statement_helper.rb
@@ -7,14 +7,14 @@ module FinancialStatementHelper
   
   def is_visible_on_report( account, branch_id )
     # 削除された科目は表示しない
-    return false if account.deleted
+    return false if account.deleted?
     
     # 決算書科目以外は表示しない
     return false unless account.is_settlement_report_account
     
     # 検索条件に部門指定がない場合は全社での出力なので内部取引は表示しない
     if branch_id.to_i == 0
-      return false if account.trade_type == TRADE_TYPE_INTERNAL
+      return false if account.internal_trade?
     end
 
     true

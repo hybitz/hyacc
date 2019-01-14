6d6efa7d0b2d5c22c4a2113e27fcb6ebc08dae92
ichy <ichylinux@gmail.com>
2019-01-14 13:56:07 +0900

    refactor

--- app/models/reports/dividend_received_logic.rb
+++ app/models/reports/dividend_received_logic.rb
@@ -9,32 +9,67 @@ module Reports
       ret.partially_stocks_amount = ret.partially_stocks.inject(0){|sum, d| sum + d.amount}
       ret.etc_stocks = get_etc_stocks(start_ym, end_ym)
       ret.etc_stocks_amount = ret.etc_stocks.inject(0){|sum, d| sum + d.amount}
+      ret.non_dominant_stocks = get_non_dominant_stocks(start_ym, end_ym)
       ret
     end
     
     def get_fully_owned_stocks(ym_start, ym_end)
-      get_stocks(ym_start, ym_end, SubAccount.where(:code => SUB_ACCOUNT_CODE_FULLY_OWNED_STOCKS))
+      get_stocks(ym_start, ym_end, SubAccount.where(code: SUB_ACCOUNT_CODE_FULLY_OWNED_STOCKS))
     end
     
     def get_partially_owned_stocks(ym_start, ym_end)
-      get_stocks(ym_start, ym_end, SubAccount.where(:code => SUB_ACCOUNT_CODE_PARTIALLY_OWNED_STOCKS))
+      get_stocks(ym_start, ym_end, SubAccount.where(code: SUB_ACCOUNT_CODE_PARTIALLY_OWNED_STOCKS))
     end
     
     def get_etc_stocks(ym_start, ym_end)
-      get_stocks(ym_start, ym_end, SubAccount.where(:code => SUB_ACCOUNT_CODE_ETC_STOCKS))
+      get_stocks(ym_start, ym_end, SubAccount.where(code: SUB_ACCOUNT_CODE_ETC_STOCKS))
     end
     
+    def get_non_dominant_stocks(ym_start, ym_end)
+      stocks = get_stocks(ym_start, ym_end, SubAccount.where(code: SUB_ACCOUNT_CODE_NON_DOMINANT_STOCKS))
+
+      ret = []
+      [stocks.size, 2].max.times do |i|
+        jd = stocks[i]
+        detail = DividendReceivedDetailModel.new
+        detail.amount = jd.try(:amount) || 0
+        detail.profit_amount = 0
+        ret << detail
+      end
+      ret
+    end
+
     def get_stocks(ym_start, ym_end, sub_account_id)
       stocks = JournalDetail.where(account_id: Account.where(code: ACCOUNT_CODE_DIVIDEND_RECEIVED), sub_account_id: sub_account_id).joins(:journal).where("journals.ym >= ? and journals.ym <= ?", ym_start, ym_end)      
     end
   end
 
   class DividendReceivedModel
-    attr_accessor :fully_owned_stocks
-    attr_accessor :partially_stocks
-    attr_accessor :etc_stocks
-    attr_accessor :fully_owned_stocks_amount
-    attr_accessor :partially_stocks_amount
-    attr_accessor :etc_stocks_amount
+    attr_accessor :fully_owned_stocks, :fully_owned_stocks_amount
+    attr_accessor :partially_stocks, :partially_stocks_amount
+    attr_accessor :etc_stocks, :etc_stocks_amount
+    attr_accessor :non_dominant_stocks
+
+    def non_dominant_amount
+      non_dominant_stocks.inject(0){|sum, d| sum + d.amount}
+    end
+    
+    def non_dominant_profit_amount
+      non_dominant_stocks.inject(0){|sum, d| sum + d.profit_amount}
+    end
+
+    def non_dominant_total_amount
+      non_dominant_stocks.inject(0){|sum, d| sum + d.total_amount}
+    end
+  end
+
+  class DividendReceivedDetailModel
+    attr_accessor :note
+    attr_accessor :amount
+    attr_accessor :profit_amount
+
+    def total_amount
+      amount - profit_amount
+    end
   end
 end

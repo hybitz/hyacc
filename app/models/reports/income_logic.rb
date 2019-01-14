87ac3525429b2e85ed9e497d9e644b3bc624a9ce
ichy <ichylinux@gmail.com>
2019-01-14 21:38:13 +0900

    refs #所得の金額の計算に関する明細書

--- app/models/reports/income_logic.rb
+++ app/models/reports/income_logic.rb
@@ -3,12 +3,16 @@ module Reports
 
     def build_model
       ret = IncomeModel.new
-      ret.company_name = Company.find(finder.company_id).name
+      ret.company = Company.find(finder.company_id)
+      ret.fiscal_year = ret.company.get_fiscal_year(finder.fiscal_year)
       ret.pretax_profit_amount = get_pretax_profit_amount
       ret.corporate_tax_amount = get_corporate_tax_amount
       
       se_logic = SocialExpenseLogic.new(finder)
-      ret.nondiductible_social_expense_amount = se_logic.get_social_expense_model.get_not_loss
+      ret.non_deductible_social_expense_amount = se_logic.get_social_expense_model.get_not_loss
+
+      dr_logic = DividendReceivedLogic.new(finder)
+      ret.dividend_received = dr_logic.build_model
       
       ret
     end
@@ -16,42 +20,81 @@ module Reports
   end
 
   class IncomeModel
-    attr_accessor :company_name
+    attr_accessor :company
+    attr_accessor :fiscal_year
     attr_accessor :pretax_profit_amount
     attr_accessor :corporate_tax_amount
-    attr_accessor :nondiductible_social_expense_amount
+    attr_accessor :non_deductible_social_expense_amount
+    attr_accessor :dividend_received
+
+    def company_name
+      company.name
+    end
     
+    def pretax_profit_retained_amount
+      0
+    end
+    
+    def pretax_profit_outflow_amount
+      0
+    end
+
     def increase_amount
       corporate_tax_amount
     end
     
-    def decrease_amount
+    def increase_retained_amount
+      corporate_tax_amount
+    end
+
+    def increase_outflow_amount
       0
     end
+
+    def decrease_amount
+      fiscal_year.accepted_amount_of_excess_depreciation + 
+        dividend_received.non_deductible_amount + 
+        fiscal_year.approved_loss_amount_of_business_tax
+    end
     
+    def decrease_retained_amount
+      fiscal_year.accepted_amount_of_excess_depreciation + 
+        fiscal_year.approved_loss_amount_of_business_tax
+    end
+    
+    def decrease_outflow_amount
+      dividend_received.non_deductible_amount 
+    end
+
     # 仮計
-    def provisional_total_amount
-      @pretax_profit_amount + increase_amount - decrease_amount
+    def provisional_amount
+      pretax_profit_amount + increase_amount - decrease_amount
     end
     
-    # 合計
-    def total_amount
-      provisional_total_amount
+    # 仮計（留保）
+    def provisional_retained_amount
+      pretax_profit_retained_amount + increase_retained_amount - decrease_retained_amount
     end
-    
-    # 総計
-    def grand_total_amount
-      total_amount
+
+    # 仮計（社外流出）
+    def provisional_outflow_amount
+      pretax_profit_outflow_amount + increase_outflow_amount - decrease_outflow_amount
     end
-    
-    # 差引計
-    def balance_amount
-      grand_total_amount
-    end
-    
+
     # 所得金額又は欠損金額
     def income_amount
-      grand_total_amount
+      provisional_amount
     end
+    
+    # 所得金額又は欠損金額（留保）
+    def income_retained_amount
+      provisional_retained_amount
+    end
+
+    # 所得金額又は欠損金額（社外流出）
+    def income_outflow_amount
+      provisional_outflow_amount
+    end
+
   end
 end

0507c9f8759956523d452b48f42076ef8d8befd0
ichy <ichylinux@gmail.com>
2019-01-20 01:32:25 +0900

    課税売上割合・控除対象仕入税額等の計算表

--- app/models/reports/consumption_tax_calc_logic.rb
+++ app/models/reports/consumption_tax_calc_logic.rb
@@ -6,6 +6,9 @@ module Reports
 
       ret.company = Company.find(finder.company_id)
       ret.fiscal_year = fiscal_year
+      ret.sale_amount = get_this_term_amount(ACCOUNT_CODE_SALE)
+      ret.temp_pay_tax_amount = get_this_term_debit_amount(ACCOUNT_CODE_TEMP_PAY_TAX)
+      ret.consumption_tax_payable_amount = 0 # TODO 中間申告した消費税額
       
       ret
     end
@@ -15,9 +18,20 @@ module Reports
   class ConsumptionTaxCalcModel
     attr_accessor :company
     attr_accessor :fiscal_year
+    attr_accessor :sale_amount
+    attr_accessor :temp_pay_tax_amount
+    attr_accessor :consumption_tax_payable_amount
 
     def company_name
       company.name
+    end
+    
+    def total_sale_amount
+      sale_amount
+    end
+    
+    def expense_amount
+      (temp_pay_tax_amount - consumption_tax_payable_amount) * 1.08 / 0.08
     end
     
   end

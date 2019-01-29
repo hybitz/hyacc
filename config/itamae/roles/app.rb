f2018f18b1dd39ea9bd2a91a86a7c765f6cc87bd
ichy <ichylinux@gmail.com>
2019-01-30 00:39:39 +0900

    refactor

--- config/itamae/roles/app.rb
+++ config/itamae/roles/app.rb
@@ -1,5 +1,7 @@
 require 'daddy/itamae'
 
+include_recipe 'daddy::wkhtmltopdf'
+
 include_recipe 'daddy::nginx::stop'
 include_recipe 'daddy::nginx::install'
 include_recipe 'daddy::nginx::start'
--- config/itamae/roles/test.rb
+++ config/itamae/roles/test.rb
@@ -1,3 +1,4 @@
 require 'daddy/itamae'
 
 include_recipe 'daddy::selenium::install'
+include_recipe 'daddy::wkhtmltopdf'

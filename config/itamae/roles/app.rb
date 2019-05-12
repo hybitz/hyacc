efbfa4787a4bb84f7a4eab2a7bbbe7ed7a6da9fe
ichy <ichylinux@gmail.com>
2019-05-12 21:49:21 +0900

    refactor

--- config/itamae/roles/app.rb
+++ config/itamae/roles/app.rb
@@ -1,7 +1,4 @@
 require 'daddy/itamae'
 
 include_recipe 'daddy::wkhtmltopdf'
-
-include_recipe 'daddy::nginx::stop'
-include_recipe 'daddy::nginx::install'
-include_recipe 'daddy::nginx::start'
+include_recipe 'daddy::nginx'

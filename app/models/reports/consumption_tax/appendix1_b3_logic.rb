4d1c4cf81012e3ef6f98fae0ce6f039905fa5ae7
ichy <ichylinux@gmail.com>
2021-01-10 17:51:55 +0900

    付表1-3 税率別消費税額計算表　兼　地方消費税の課税標準となる消費税額計算表

--- app/models/reports/consumption_tax/appendix1_b3_logic.rb
+++ app/models/reports/consumption_tax/appendix1_b3_logic.rb
@@ -1,31 +1,36 @@
-class Reports::ConsumptionTax::Appendix1B3Logic < Reports::BaseLogic
+module Reports
+  module ConsumptionTax
+    class Appendix1B3Logic < Reports::BaseLogic
 
-  def build_model
-    ret = HeaderModel.new
+      def build_model
+        ret = Appendix1B3Model.new
+    
+        ret.company = Company.find(finder.company_id)
+        ret.fiscal_year = fiscal_year
+        ret.sale_amount_raw = get_this_term_amount(ACCOUNT_CODE_SALE)
+        ret
+      end
 
-    ret.company = Company.find(finder.company_id)
-    ret.fiscal_year = fiscal_year
-    ret.sale_amount_raw = get_this_term_amount(ACCOUNT_CODE_SALE)
-    ret
+    end
+
+    class Appendix1B3Model
+      attr_accessor :company
+      attr_accessor :fiscal_year
+      attr_accessor :sale_amount_raw
+    
+      def company_name
+        company.name
+      end
+    
+      def sale_amount
+        sale_amount_raw - sale_amount_raw % 1000
+      end
+    
+      def sale_tax_amount
+        sale_amount * 0.078
+      end
+    
+    end
+
   end
-
-end
-
-class HeaderModel
-  attr_accessor :company
-  attr_accessor :fiscal_year
-  attr_accessor :sale_amount_raw
-
-  def company_name
-    company.name
-  end
-
-  def sale_amount
-    sale_amount_raw - sale_amount_raw % 1000
-  end
-
-  def sale_tax_amount
-    sale_amount * 0.078
-  end
-
 end

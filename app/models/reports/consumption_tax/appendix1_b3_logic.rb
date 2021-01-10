9223d9031c7c50fee5d57dde0d9bafec7025bb49
ichy <ichylinux@gmail.com>
2021-01-10 16:44:23 +0900

    付表1-3 税率別消費税額計算表　兼　地方消費税の課税標準となる消費税額計算表

--- app/models/reports/consumption_tax/appendix1_b3_logic.rb
+++ app/models/reports/consumption_tax/appendix1_b3_logic.rb
@@ -5,6 +5,7 @@ class Reports::ConsumptionTax::Appendix1B3Logic < Reports::BaseLogic
 
     ret.company = Company.find(finder.company_id)
     ret.fiscal_year = fiscal_year
+    ret.sale_amount_raw = get_this_term_amount(ACCOUNT_CODE_SALE)
     ret
   end
 
@@ -13,9 +14,18 @@ end
 class HeaderModel
   attr_accessor :company
   attr_accessor :fiscal_year
+  attr_accessor :sale_amount_raw
 
   def company_name
     company.name
   end
-  
+
+  def sale_amount
+    sale_amount_raw - sale_amount_raw % 1000
+  end
+
+  def sale_tax_amount
+    sale_amount * 0.078
+  end
+
 end

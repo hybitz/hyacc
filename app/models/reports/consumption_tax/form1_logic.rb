2b0e497e598afb02d2b9a63aa6afe5a806a57815
ichy <ichylinux@gmail.com>
2021-01-11 14:17:34 +0900

    第一表　消費税及び地方消費税の申告書（一般用）

---
+++ app/models/reports/consumption_tax/form1_logic.rb
@@ -0,0 +1,23 @@
+module Reports
+  module ConsumptionTax
+    class Form1Logic < BaseLogic
+
+      def build_model
+        ret = Form1Model.new
+    
+        ret.company = Company.find(finder.company_id)
+        ret.fiscal_year = fiscal_year
+        ret.sale_amount = get_this_term_amount(ACCOUNT_CODE_SALE)
+        ret.reduced_sale_amount = 0
+        ret.taxable_purchase_amount = get_taxable_purchase_amount(0.1)
+        ret.reduced_taxable_purchase_amount = get_taxable_purchase_amount(0.08)
+        ret
+      end
+
+    end
+
+    class Form1Model < BaseModel
+    end
+
+  end
+end

dc91bf17eb0b2d0af1caed80e41b85ac1a323cc8
ichy <ichylinux@gmail.com>
2021-01-13 00:14:58 +0900

    refactor

--- test/controllers/journals_controller/corporate_tax_test.rb
+++ test/controllers/journals_controller/corporate_tax_test.rb
@@ -9,7 +9,7 @@ class JournalsController::CorporateTaxTest < ActionController::TestCase
   
   def test_create_allocated_tax_cost
     assert a = Account.find_by_code(ACCOUNT_CODE_CORPORATE_TAXES)
-    assert sa = SubAccount.where(:sub_account_type => SUB_ACCOUNT_TYPE_CORPORATE_TAX, :code => '200').first
+    assert sa = SubAccount.find_by(sub_account_type: SUB_ACCOUNT_TYPE_CORPORATE_TAX, code: '200')
 
     post_jh = Journal.new
     post_jh.remarks = '法人税等の決済区分テスト' + Time.now.to_s

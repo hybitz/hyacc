4ca0cf7283f7ea050fdbf2fa9da1b90077da1352
ichy <ichylinux@gmail.com>
2022-01-19 01:36:58 +0900

    refactor

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module Hyacc
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
+    config.i18n.fallbacks = [:ja, :en]
     config.i18n.default_locale = :ja
 
     config.action_controller.action_on_unpermitted_parameters = :raise

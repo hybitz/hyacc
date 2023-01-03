6d2e3dd501bca636208ecadb6315b8367efba1c1
ichy <ichylinux@gmail.com>
2023-01-03 20:36:53 +0900

    無効な従業員の表示を改善

--- app/models/employee_finder.rb
+++ app/models/employee_finder.rb
@@ -4,6 +4,6 @@ class EmployeeFinder
   include CompanyAware
 
   def list
-    Employee.where(company_id: company_id, deleted: false).paginate(:page => page, :per_page => per_page)
+    Employee.where(company_id: company_id).paginate(page: page, per_page: per_page)
   end
 end

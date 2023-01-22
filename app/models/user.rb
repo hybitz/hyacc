81f172cf6a65afdbb452c73dcee4bc4198bb3c19
ichy <ichylinux@gmail.com>
2023-01-23 01:37:55 +0900

    refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_one_time_password
 
   validates :login_id, presence: true, uniqueness: true
-  validates_format_of :login_id, :with=>/[a-zA-Z0-9._]*/
+  validates_format_of :login_id, :with=>/\A[a-zA-Z0-9._]+\z/
   validates_format_of :password, :with=>/\A[!-~]*\z/
   validates_format_of :email, allow_nil: true, allow_blank: true, with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\z/
   validates_format_of :google_account, allow_nil: true, allow_blank: true, with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\z/

56aecf869ae9ed9bb092f9e04066f4c8cd5fe18f
ichy <ichylinux@gmail.com>
2024-05-02 13:08:27 +0900

    その他調整額の対応開始

--- test/models/payroll_test.rb
+++ test/models/payroll_test.rb
@@ -10,4 +10,24 @@ class PayrollTest < ActiveSupport::TestCase
     employee = @payroll.employee
     assert_equal 394000, Payroll.get_previous(date, employee.id).base_salary
   end
+
+  def test_その他調整額
+    e = employee
+    fy = e.company.fiscal_years.find_or_initialize_by(fiscal_year: 2024)
+    fy.closing_status = CLOSING_STATUS_OPEN
+    assert fy.save, fy.errors.full_messages
+    
+    p = Payroll.new(ym: 202404, pay_day: '2024-05-07', employee: e, base_salary: 300_000, monthly_standard: 300_000)
+    p.create_user_id = p.update_user_id = e.id
+    p.misc_adjustment_note = 'その他調整額テスト'
+    p.misc_adjustment = 50_000
+    assert p.save, p.errors.full_messages
+
+    assert pd = p.payroll_journal
+    pd.journal_details.each do |jd|
+      puts
+      puts jd.attributes.sort.map(&:to_s)
+    end
+  end
+
 end

ed5537f8603772329063670dfd0275a939d1b5c0
ichy <ichylinux@gmail.com>
2024-05-26 15:32:42 +0900

    refactor

--- app/models/depreciation/strategy/lump.rb
+++ app/models/depreciation/strategy/lump.rb
@@ -3,8 +3,10 @@ module Depreciation::Strategy
   class Lump < Depreciation::Strategy::Base
 
     def create_depreciations(asset)
+      c = asset.branch.company
+
       d = asset.depreciations.build
-      d.fiscal_year = asset.branch.company.get_fiscal_year_int(asset.ym)
+      d.fiscal_year = c.get_fiscal_year_int(asset.ym)
       d.amount_at_start = asset.amount
       d.amount_at_end = 0
     end

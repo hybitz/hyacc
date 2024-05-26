d2637f04d3e557bc2bf85552f497c4eacfa8b938
ichy <ichylinux@gmail.com>
2024-05-26 16:26:02 +0900

    refactor

--- test/controllers/bs/assets_controller_test.rb
+++ test/controllers/bs/assets_controller_test.rb
@@ -11,28 +11,28 @@ class Bs::AssetsControllerTest < ActionController::TestCase
 
   def test_参照
     sign_in user
-    get :show, :params => {:id => asset.id}, :xhr => true
+    get :show, params: {id: asset.id}, xhr: true
     assert_response :success
     assert_template :show
   end
 
   def test_編集
     sign_in user
-    get :edit, :params => {:id => asset.id}, :xhr => true
+    get :edit, params: {id: asset.id}, xhr: true
     assert_response :success
     assert_template :edit
   end
 
   def test_更新
     sign_in user
-    patch :update, :params => {:id => asset.id, :asset => valid_asset_params}, :xhr => true
+    patch :update, params: {id: asset.id, asset: valid_asset_params}, xhr: true
     assert_response :success
     assert_template 'common/reload'
   end
 
   def test_更新_入力エラー
     sign_in user
-    patch :update, :params => {:id => asset.id, :asset => invalid_asset_params}, :xhr => true
+    patch :update, params: {id: asset.id, asset: invalid_asset_params}, xhr: true
     assert_response :success
     assert_template :edit
   end

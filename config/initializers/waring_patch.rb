591fbdccafa453c341a98dba05fc73b23d2f0a64
ichy <ichylinux@gmail.com>
2024-11-07 23:05:13 +0900

    refactor

--- config/initializers/waring_patch.rb
+++
@@ -1,9 +0,0 @@
-rb_version = Gem::Version.new(RUBY_VERSION)
-if rb_version >= Gem::Version.new('2.7') && Gem::Version.new(Rails.version) < Gem::Version.new('6')
-  module Warning
-    def warn(str)
-      return if str.match?('/gems/')
-      super
-    end
-  end
-end

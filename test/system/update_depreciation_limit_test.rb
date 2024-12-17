8a8d8cc1bfa3783cf0c86239251055f4ec8a28ea
N2Y <aoaki202320@gmail.com>
2024-12-17 16:02:04 +0900

    refs #42 償却限度額の変更に対応

--- test/system/update_depreciation_limit_test.rb
+++ test/system/update_depreciation_limit_test.rb
@@ -11,17 +11,17 @@ class UpdateDepreciationLimitTest < ApplicationSystemTestCase
     visit bs_assets_path
     click_on "編集"
     within(".ui-dialog") do
-      assert_equal DEPRECIATION_METHOD_FIXED_RATE, find("#asset_depreciation_method").value.to_i
-      assert_equal 1, find("#asset_depreciation_limit").value.to_i
+      assert_equal DEPRECIATION_METHOD_FIXED_RATE, page.find("#asset_depreciation_method").value.to_i
+      assert_equal 1, page.find("#asset_depreciation_limit").value.to_i
       select '一括償却', from: 'asset[depreciation_method]'
-      assert_equal 0, find("#asset_depreciation_limit").value.to_i
+      assert_equal 0, page.find("#asset_depreciation_limit").value.to_i
       click_on '更新'
     end
 
     click_on '編集'
     within(".ui-dialog") do
-      assert_equal DEPRECIATION_METHOD_LUMP, find("#asset_depreciation_method").value.to_i
-      assert_equal 0, find("#asset_depreciation_limit").value.to_i
+      assert_equal DEPRECIATION_METHOD_LUMP, page.find("#asset_depreciation_method").value.to_i
+      assert_equal 0, page.find("#asset_depreciation_limit").value.to_i
       click_on '閉じる'
     end
   end
@@ -32,17 +32,38 @@ class UpdateDepreciationLimitTest < ApplicationSystemTestCase
     visit bs_assets_path
     click_on "編集"
     within(".ui-dialog") do
-      assert_equal DEPRECIATION_METHOD_FIXED_RATE, find("#asset_depreciation_method").value.to_i
-      assert_equal 2, find("#asset_depreciation_limit").value.to_i
+      assert_equal DEPRECIATION_METHOD_FIXED_RATE, page.find("#asset_depreciation_method").value.to_i
+      assert_equal 2, page.find("#asset_depreciation_limit").value.to_i
       select '一括償却', from: 'asset[depreciation_method]'
-      assert_equal 2, find("#asset_depreciation_limit").value.to_i
+      assert_equal 2, page.find("#asset_depreciation_limit").value.to_i
       click_on '更新'
     end
 
     click_on '編集'
     within(".ui-dialog") do
-      assert_equal DEPRECIATION_METHOD_LUMP, find("#asset_depreciation_method").value.to_i
-      assert_equal 2, find("#asset_depreciation_limit").value.to_i
+      assert_equal DEPRECIATION_METHOD_LUMP, page.find("#asset_depreciation_method").value.to_i
+      assert_equal 2, page.find("#asset_depreciation_limit").value.to_i
+      click_on '閉じる'
+    end
+  end
+
+  def test_定率法のまま償却限度額を1円以外に変更した後に一括償却ヘ変更した場合は自動変換されない
+    visit bs_assets_path
+    click_on "編集"
+    within(".ui-dialog") do
+      assert_equal DEPRECIATION_METHOD_FIXED_RATE, page.find("#asset_depreciation_method").value.to_i
+      assert_equal 1, page.find("#asset_depreciation_limit").value.to_i
+
+      fill_in 'asset[depreciation_limit]', with: 100
+      select '一括償却', from: 'asset[depreciation_method]'
+      assert_equal 100, page.find("#asset_depreciation_limit").value.to_i
+      click_on '更新'
+    end
+
+    click_on '編集'
+    within(".ui-dialog") do
+      assert_equal DEPRECIATION_METHOD_LUMP, page.find("#asset_depreciation_method").value.to_i
+      assert_equal 100, page.find("#asset_depreciation_limit").value.to_i
       click_on '閉じる'
     end
   end

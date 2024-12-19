a667e524051caa31a906ee0dec2f11eddcc15317
ichy <ichylinux@gmail.com>
2024-12-19 19:59:28 +0900

    refs #42 償却限度額の変換に焦点をあてるようにシナリオをリファクタリング

--- features/step_definitions/assets.rb
+++ features/step_definitions/assets.rb
@@ -1,8 +1,13 @@
-前提 /^減価償却の一覧を表示している$/ do
+前提 /^資産の編集画面を表示している$/ do
   visit '/bs'
   click_on '減価償却'
-  assert_url '/bs/assets$'
   assert page.has_selector?('table#asset_container')
+  capture
+  click_on '編集'
+  assert wait_until { has_selector?("div.ui-dialog", visible: true) }
+  assert has_selector?("span.ui-dialog-title", text: /減価償却　編集/)
+ensure
+  capture
 end
 
 もし /^資産管理の減価償却をクリックする$/ do
@@ -15,10 +20,6 @@ end
   assert_url '/bs/assets$'
 end
 
-もし /^減価償却の一覧から編集をクリックする$/ do
-  click_on '編集'
-end
-
 もし /^.*償却限度額を(.*?)円に変更した状態で、償却方法を(.*?)に変更する$/ do |limit, method|
   fill_in 'asset[depreciation_limit]', with: limit
   select method, from: 'asset[depreciation_method]'
@@ -26,11 +27,6 @@ end
 
 もし /^.*償却方法を(.*?)に変更する$/ do |method|
   select method, from: 'asset[depreciation_method]'
-end
-
-ならば /^資産の編集ダイアログが表示される$/ do
-  assert wait_until { has_selector?("div.ui-dialog", :visible => true) }
-  assert has_selector?("span.ui-dialog-title", :text => /減価償却　編集/)
 end
 
 ならば /^.*償却限度額は(.*?)円(に変換される|のまま)$/ do |limit, text|

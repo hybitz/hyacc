177f7bd61a05eef8c4b88d4289dfa9cdfec307dc
N2Y <aoaki202320@gmail.com>
2024-12-26 11:12:10 +0900

    refs #42 フィーチャファイルの限度額の例を0円.1円.2円以上に整理/リファクタリング

--- features/step_definitions/assets.rb
+++ features/step_definitions/assets.rb
@@ -22,29 +22,25 @@ end
 
 もし /^償却方法を変更した時、償却限度額は以下のように自動変更される$/ do |ast_table|
   rows = normalize_table(ast_table)
-  rows.shift
-  details = []
-  rows.each do |r|
-    details << r
-  end
-  
-  details.each do |d|
-    current_method = d[0]
-    current_limit = d[1]
-    new_method = d[2]
-    new_limit = d[3]
+
+  rows[1..-1].each do |r|
+    current_method = r[0]
+    current_limit = r[1]
+    new_method = r[2]
+    new_limit = r[3]
 
     select current_method, from: 'asset[depreciation_method]'
 
     case current_limit
     when "0円"
       fill_in 'asset[depreciation_limit]', with: 0
+      current_limit = 0
     when "1円"
       fill_in 'asset[depreciation_limit]', with: 1
       current_limit = 1
-    when "1円以外", "0円以外"
-      fill_in 'asset[depreciation_limit]', with: 10
-      current_limit = 10
+    else
+      fill_in 'asset[depreciation_limit]', with: 2
+      current_limit = 2
     end
 
     select new_method, from: 'asset[depreciation_method]'
@@ -52,7 +48,9 @@ end
     case new_limit
     when "0円にする"
       assert_equal 0, find("#asset_depreciation_limit").value.to_i
-    when "1円のまま変更しない", "変更しない"
+    when "1円にする"
+      assert_equal 1, find("#asset_depreciation_limit").value.to_i
+    else
       assert_equal current_limit, find("#asset_depreciation_limit").value.to_i
     end
   end

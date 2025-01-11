a299a2e8202f2b0958a9d600e754693233f4ba81
ichy <ichylinux@gmail.com>
2025-01-11 15:03:38 +0900

    別表１

---
+++ app/models/reports/appendix_01_logic.rb
@@ -0,0 +1,13 @@
+module Reports
+  class Appendix01Logic < BaseLogic
+
+    def build_model
+      ret = Appendix01Model.new
+      ret
+    end
+
+  end
+
+  class Appendix01Model
+  end
+end
---
+++ app/models/reports/appendix_01_next_logic.rb
@@ -0,0 +1,13 @@
+module Reports
+  class Appendix01NextLogic < BaseLogic
+
+    def build_model
+      ret = Appendix01NextModel.new
+      ret
+    end
+
+  end
+
+  class Appendix01NextModel
+  end
+end

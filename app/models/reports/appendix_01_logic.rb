3f08cecb510c18c207b9ce370a38587c8f79b1fe
ichy <ichylinux@gmail.com>
2025-01-14 02:58:12 +0900

    別表１

--- app/models/reports/appendix_01_logic.rb
+++ app/models/reports/appendix_01_logic.rb
@@ -3,11 +3,20 @@ module Reports
 
     def build_model
       ret = Appendix01Model.new
+
+      appendix_04_logic = Appendix04Logic.new(finder)
+      ret.appendix_04_model = appendix_04_logic.build_model
       ret
     end
 
   end
 
   class Appendix01Model
+    attr_accessor :appendix_04_model
+
+    # 所得金額又は欠損金額
+    def income_amount
+      appendix_04_model.income_amount
+    end
   end
 end

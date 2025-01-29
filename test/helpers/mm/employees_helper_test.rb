14b3a409b06c08818495eca286a23100601d1c32
N2Y <aoaki202320@gmail.com>
2025-01-29 17:33:50 +0900

    refs #44 ヘルパーメソッドget_start_ym_of_retirement_savingsのテストケースを追加

--- test/helpers/mm/employees_helper_test.rb
+++ test/helpers/mm/employees_helper_test.rb
@@ -7,5 +7,22 @@ class Mm::EmployeesHelperTest < ActionView::TestCase
     assert_equal '4ヶ月', render_duration(0, 4)
     assert_equal '2年7ヶ月', render_duration(2, 7)
   end
+ 
+  def test_get_start_ym_of_retirement_savings
+    employee = Employee.first
+    company = Company.find(employee.company_id)
+    assert_equal "2009年1月", employee.employment_date.strftime("%Y年%-m月")
+    assert_nil company.retirement_savings_after
+    assert_nil get_start_ym_of_retirement_savings(employee)
+
+    company.update!(retirement_savings_after: 1)
+    assert_equal "2009年1月", get_start_ym_of_retirement_savings(employee)
+
+    company.update!(retirement_savings_after: 2)
+    assert_equal "2010年1月", get_start_ym_of_retirement_savings(employee)
+
+    company.update!(retirement_savings_after: 3)
+    assert_equal "2011年1月", get_start_ym_of_retirement_savings(employee)
+  end
 
 end

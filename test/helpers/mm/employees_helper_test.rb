66ce2d1c2ea62fb661d2f2eb81e98d54a8194853
N2Y <aoaki202320@gmail.com>
2025-01-31 11:53:25 +0900

    （修正）Revert "refs #44 test_get_start_ym_of_retirement_savingsの入社月の例を追加"
    This reverts commit 715dc9fcaa00370d480209b69f66cb1a7c30ef56.

--- test/helpers/mm/employees_helper_test.rb
+++ test/helpers/mm/employees_helper_test.rb
@@ -9,17 +9,21 @@ class Mm::EmployeesHelperTest < ActionView::TestCase
   end
  
   def test_get_start_ym_of_retirement_savings
-    employee = Employee.first
-    employee.company.update!(retirement_savings_after: 3)
+    company = Company.first
+    company.update!(retirement_savings_after: 1)
+    employees = Employee.where(company_id: company.id)
+    employees[0].update!(employment_date: '20200101')
+    employees[1].update!(employment_date: '20210201')
+    employees[2].update!(employment_date: '20220701')
+    assert_equal ['2020年1月', '2021年2月', '2022年7月'], [get_start_ym_of_retirement_savings(employees[0]), get_start_ym_of_retirement_savings(employees[1]), get_start_ym_of_retirement_savings(employees[2])]
 
-    employee.update!(employment_date: '20200101')
-    assert_equal '2022年1月', get_start_ym_of_retirement_savings(employee)
-
-    employee.update!(employment_date: '20200201')
-    assert_equal '2022年2月', get_start_ym_of_retirement_savings(employee)
-
-    employee.update!(employment_date: '20200701')
-    assert_equal '2022年7月', get_start_ym_of_retirement_savings(employee)
+    company.update!(retirement_savings_after: 2)
+    employees = Employee.where(company_id: company.id)
+    assert_equal ['2021年1月', '2022年2月', '2023年7月'],  [get_start_ym_of_retirement_savings(employees[0]), get_start_ym_of_retirement_savings(employees[1]), get_start_ym_of_retirement_savings(employees[2])]
+    
+    company.update!(retirement_savings_after: 3)
+    employees = Employee.where(company_id: company.id)
+    assert_equal ['2022年1月', '2023年2月', '2024年7月'],  [get_start_ym_of_retirement_savings(employees[0]), get_start_ym_of_retirement_savings(employees[1]), get_start_ym_of_retirement_savings(employees[2])]
   end
 
 end

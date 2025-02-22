7f647a46057764ceeba31c418314e5f9f09b3345
ichy <ichylinux@gmail.com>
2025-02-22 10:23:46 +0900

    リファクタリング

--- test/models/auto/journal/investment_factory_test.rb
+++ test/models/auto/journal/investment_factory_test.rb
@@ -5,7 +5,7 @@ class Auto::Journal::InvestmentFactoryTest < ActiveSupport::TestCase
   # 有価証券の購入に手数料がかからない場合のテスト
   def test_make_journals_no_charges
     @investment = Investment.new(yyyymmdd: '2016-03-27', bank_account_id: 3, customer_id: '1',
-    buying_or_selling: '1', for_what: '1', shares: '20', trading_value: '100000', charges: '0')
+      buying_or_selling: '1', for_what: '1', shares: '20', trading_value: '100000', charges: '0')
     factory = Auto::Journal::InvestmentFactory.get_instance(Auto::Journal::InvestmentParam.new(@investment, user))
     journals = factory.make_journals
 
@@ -18,16 +18,16 @@ class Auto::Journal::InvestmentFactoryTest < ActiveSupport::TestCase
 
     jd = jh.journal_details[0]
     assert_equal Account.find_by_code(ACCOUNT_CODE_TRADING_SECURITIES).id, jd.account_id
-    assert_equal 100000, jd.amount
+    assert_equal 100_000, jd.amount
     
     jd = jh.journal_details[1]
     assert_equal Account.find_by_code(ACCOUNT_CODE_DEPOSITS_PAID).id, jd.account_id
-    assert_equal 100000, jd.amount
+    assert_equal 100_000, jd.amount
   end
 
   def test_make_journals
     @investment = Investment.new(yyyymmdd: '2016-03-27', bank_account_id: 3, customer_id: '1',
-    buying_or_selling: '1', for_what: '1', shares: '20', trading_value: '100000', charges: '1080')
+      buying_or_selling: '1', for_what: '1', shares: '20', trading_value: '100000', charges: '1080')
     factory = Auto::Journal::InvestmentFactory.get_instance(Auto::Journal::InvestmentParam.new(@investment, user))
     journals = factory.make_journals
 
@@ -40,7 +40,7 @@ class Auto::Journal::InvestmentFactoryTest < ActiveSupport::TestCase
 
     jd = jh.journal_details[0]
     assert_equal Account.find_by_code(ACCOUNT_CODE_TRADING_SECURITIES).id, jd.account_id
-    assert_equal 100000, jd.amount
+    assert_equal 100_000, jd.amount
 
     jd = jh.journal_details[1]
     assert_equal Account.find_by_code(ACCOUNT_CODE_TEMP_PAY_TAX).id, jd.account_id
@@ -52,6 +52,6 @@ class Auto::Journal::InvestmentFactoryTest < ActiveSupport::TestCase
 
     jd = jh.journal_details[3]
     assert_equal Account.find_by_code(ACCOUNT_CODE_DEPOSITS_PAID).id, jd.account_id
-    assert_equal 101080, jd.amount
+    assert_equal 101_080, jd.amount
   end
 end

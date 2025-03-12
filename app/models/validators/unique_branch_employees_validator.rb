27295990e3a49c3574e5c108028d105d2056c981
N2Y <aoaki202320@gmail.com>
2025-03-12 15:30:26 +0900

    refs #75 所属部門のデフォルト設定を重複して登録しないように修正する

--- app/models/validators/unique_branch_employees_validator.rb
+++ app/models/validators/unique_branch_employees_validator.rb
@@ -3,12 +3,16 @@ module Validators
   class UniqueBranchEmployeesValidator < ActiveModel::Validator
 
     def validate(record)
-      return unless record.branch_employees
-
       branch_employees = record.branch_employees.select {|be| ! be.deleted? }
-      return unless branch_employees
+      return if branch_employees.empty?
+      validate_branch(record, branch_employees)
+      validate_default_branch(record, branch_employees)
+    end
 
-      branch_ids = []      
+    private
+
+    def validate_branch(record, branch_employees)
+      branch_ids = []
       branch_employees.each do |be|
         if branch_ids.include?(be.branch_id)
           record.errors.add(:base, I18n.t('errors.messages.branch_employees_duplicated'))
@@ -16,6 +20,12 @@ module Validators
         end
 
         branch_ids << be.branch_id
+      end
+    end
+
+    def validate_default_branch(record, branch_employees)
+      if branch_employees.select {|be| be.default_branch? }.size > 1
+        record.errors.add(:base, I18n.t('errors.messages.default_branches_duplicated'))
       end
     end
 
--- test/models/validators/unique_branch_employees_validator_test.rb
+++ test/models/validators/unique_branch_employees_validator_test.rb
@@ -1,18 +1,20 @@
 require 'test_helper'
 
 class UniqueBranchEmployeesValidatorTest < ActiveSupport::TestCase
+  def setup
+    @branch_id = employee.branch_employees.first.branch_id
+    @new_branch_id = BranchEmployee.where.not(employee_id: employee.id).first.branch_id
+  end
 
   def test_所属部門の重複チェック
-    assert_not employee.branch_employees.where(deleted: false).find{|be| be.branch_id == 2 }.present?
-    employee.branch_employees.build(branch_id: 2)
+    employee.branch_employees.build(branch_id: @new_branch_id)
     assert employee.valid?
     assert_difference 'BranchEmployee.count', 1 do
       assert employee.save
     end
     assert employee.errors.empty?
 
-    assert employee.branch_employees.where(deleted: false).find{|be| be.branch_id == branch.id }.present?
-    employee.branch_employees.build(branch_id: branch.id, deleted: true)
+    employee.branch_employees.build(branch_id: @branch_id, deleted: true)
     assert employee.valid?
     assert_difference 'BranchEmployee.count', 1 do
       assert_no_difference 'BranchEmployee.where(deleted: false).count' do
@@ -21,11 +23,40 @@ class UniqueBranchEmployeesValidatorTest < ActiveSupport::TestCase
     end
     assert employee.errors.empty?
 
-    employee.branch_employees.build(branch_id: branch.id)
+    employee.branch_employees.build(branch_id: @branch_id)
     assert_not employee.valid?
     assert_no_difference 'BranchEmployee.count' do
       assert_not employee.save
     end
+    assert employee.errors[:base].include?(I18n.t('errors.messages.branch_employees_duplicated'))
+  end
+
+  def test_所属部門のデフォルト設定の重複チェック
+    assert employee.branch_employees.find_by(branch_id: @branch_id).default_branch?
+    employee.branch_employees.build(branch_id: @branch_id, default_branch: true, deleted: true)
+    assert employee.valid?
+    assert_difference 'BranchEmployee.count', 1 do
+      assert_no_difference 'BranchEmployee.where(deleted: false).count' do
+        assert employee.save
+      end
+    end
+    assert employee.errors.empty?
+
+    employee.branch_employees.build(branch_id: @new_branch_id, default_branch: true)
+    assert_not employee.valid?
+    assert_no_difference 'BranchEmployee.count' do
+      assert_not employee.save
+    end
+    assert employee.errors[:base].include?(I18n.t('errors.messages.default_branches_duplicated'))
+
+    employee.errors.delete(:base)
+    employee.branch_employees.reload.build(branch_id: @branch_id, default_branch: true)
+    assert_not employee.valid?
+    assert_no_difference 'BranchEmployee.count' do
+      assert_not employee.save
+    end
+
+    assert employee.errors[:base].include?(I18n.t('errors.messages.branch_employees_duplicated'))
     assert employee.errors[:base].include?(I18n.t('errors.messages.branch_employees_duplicated'))
   end
 

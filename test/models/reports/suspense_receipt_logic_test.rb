6e6492e1045dcd9d7d5ea5fb305d1ef0975fe162
N2Y <aoaki202320@gmail.com>
2025-03-14 17:37:50 +0900

    refs #48 伝票と明細のテストデータを追加/テストケースを対応させる

--- test/models/reports/suspense_receipt_logic_test.rb
+++ test/models/reports/suspense_receipt_logic_test.rb
@@ -4,30 +4,28 @@ class Reports::SuspenseReceiptLogicTest < ActiveSupport::TestCase
   def setup
     @account = Account.where(is_suspense_receipt_account: true, sub_account_type: SUB_ACCOUNT_TYPE_CUSTOMER, deleted: false).first
     @fiscal_year = company.fiscal_years.where(closing_status: CLOSING_STATUS_OPEN).order('fiscal_year ASC').second
-    
-    SimpleSlip.new(params.merge(amount_increase: 10000)).save!
-    @journal1 = Journal.last
-    @journal1.journal_details.find_by(account_id: @account.id).update!(note: '不明入金1万円')
-    
-    SimpleSlip.new(params.merge(amount_increase: 20000)).save!
-    @journal2 = Journal.last
-    @journal2.journal_details.find_by(account_id: @account.id).update!(note: '不明入金2万円')
-    
-    SimpleSlip.new(params.merge(company_id: Company.where(deleted: false).second.id, ym: @fiscal_year.start_year_month - 1, amount_increase: 300000)).save!
-    @journal3 = Journal.last
-    @journal3.journal_details.find_by(account_id: @account.id).update!(note: '不明入金3万円')
+    @journal1 = Journal.find(35)
+    @journal1_jd = @journal1.journal_details.find_by(account_id: @account.id)
+    @journal2 = Journal.find(36)
+    @journal2_jd = @journal2.journal_details.find_by(account_id: @account.id)
+    @journal3 = Journal.find(37)
+    @journal3_jd = @journal3.journal_details.find_by(account_id: @account.id)
+    @journal4 = Journal.find(38)
+    @journal4_jd = @journal4.journal_details.find_by(account_id: @account.id)
   end
 
-  def test_取引先からの仮受金の摘要欄は会計年度内で金額が一番大きい伝票明細のメモを表示する 
+  def test_取引先からの仮受金の摘要欄は会計年度内で金額が一番大きい伝票明細のメモを表示する
+    @journal4.destroy!
+    assert_equal [true, true, 10000, '不明入金1万円'], [@journal1.company == company, @journal1.fiscal_year == @fiscal_year, @journal1_jd.amount, @journal1_jd.note]
+    assert_equal [true, true, 20000, '不明入金2万円'], [@journal2.company == company, @journal2.fiscal_year == @fiscal_year, @journal2_jd.amount, @journal2_jd.note]
+    assert_equal [false, false, 30000, '不明入金3万円'], [@journal3.company == company, @journal3.fiscal_year == @fiscal_year, @journal3_jd.amount, @journal3_jd.note]
+    
     finder = ReportFinder.new(user)
     finder.fiscal_year = @fiscal_year.fiscal_year
     finder.company_id = company.id
     finder.branch_id = 0
     logic = Reports::SuspenseReceiptLogic.new(finder)
-    
-    assert_equal [true, true], [@journal1.company == company, @journal1.fiscal_year == @fiscal_year]
-    assert_equal [true, true], [@journal2.company == company, @journal2.fiscal_year == @fiscal_year]
-    assert_equal [false, false], [@journal3.company == company, @journal3.fiscal_year == @fiscal_year]
+
     @model = logic.build_model
     assert_equal  ['不明入金2万円　等'], @model.details.map {|d| d.note if d.account.sub_account_type == SUB_ACCOUNT_TYPE_CUSTOMER}
  
@@ -52,10 +50,10 @@ class Reports::SuspenseReceiptLogicTest < ActiveSupport::TestCase
   end
 
   def test_取引先からの仮受金の摘要欄を部門別に表示する
-    branch2 = Branch.where(deleted: false).second
-    SimpleSlip.new(params.merge(branch_id: branch2.id, amount_increase: 100000)).save!
-    @journal4 = Journal.last
-    @journal4.journal_details.find_by(account_id: @account.id).update!(note: '不明入金10万円')
+    assert_equal [true, true, 10000, '不明入金1万円'], [@journal1.company == company, @journal1.fiscal_year == @fiscal_year, @journal1_jd.amount, @journal1_jd.note]
+    assert_equal [true, true, 20000, '不明入金2万円'], [@journal2.company == company, @journal2.fiscal_year == @fiscal_year, @journal2_jd.amount, @journal2_jd.note]
+    assert_equal [true, true, 100000, '不明入金10万円'], [@journal4.company == company, @journal4.fiscal_year == @fiscal_year, @journal4_jd.amount, @journal4_jd.note]
+    assert_equal [1, 1, 2], [@journal1_jd.branch_id, @journal2_jd.branch_id, @journal4_jd.branch_id]
 
     finder = ReportFinder.new(user)
     finder.fiscal_year = @fiscal_year.fiscal_year
@@ -69,7 +67,7 @@ class Reports::SuspenseReceiptLogicTest < ActiveSupport::TestCase
     finder = ReportFinder.new(user)
     finder.fiscal_year = @fiscal_year.fiscal_year
     finder.company_id = company.id
-    finder.branch_id = branch.id
+    finder.branch_id = 1
     logic = Reports::SuspenseReceiptLogic.new(finder)
 
     @model = logic.build_model
@@ -78,30 +76,11 @@ class Reports::SuspenseReceiptLogicTest < ActiveSupport::TestCase
     finder = ReportFinder.new(user)
     finder.fiscal_year = @fiscal_year.fiscal_year
     finder.company_id = company.id
-    finder.branch_id = branch2.id
+    finder.branch_id = 2
     logic = Reports::SuspenseReceiptLogic.new(finder)
 
     @model = logic.build_model
     assert_equal  ['不明入金10万円　等'], @model.details.map {|d| d.note if d.account.sub_account_type == SUB_ACCOUNT_TYPE_CUSTOMER}
-  end
-
-  private
-
-  def params
-    account = Account.find_by_code(ACCOUNT_CODE_SUSPENSE_RECEIPT)
-    my_account = Account.find_by_code(ACCOUNT_CODE_CASH)
-    ret = {}
-    ret[:ym] = @fiscal_year.end_year_month
-    ret[:day] = 1
-    ret[:my_account_id] = my_account.id
-    ret[:company_id] = company.id
-    ret[:create_user_id] = user.id
-    ret[:remarks] = '簡易入力テスト'
-    ret[:account_id] = account.id
-    ret[:sub_account_id] = account.sub_accounts.first.id
-    ret[:branch_id] = branch.id
-    ret[:tax_type] = TAX_TYPE_NONTAXABLE
-    ret
   end
 
 end

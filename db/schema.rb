0ab48661387f82d9a7a547ca5d96be63068b27be
N2Y <aoaki202320@gmail.com>
2025-05-13 18:22:52 +0900

    refs #103 照合順序を変更用のマイグレーションファイルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_03_09_105124) do
+ActiveRecord::Schema.define(version: 2025_05_13_055900) do
 
   create_table "accounts", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.string "code", default: "", null: false

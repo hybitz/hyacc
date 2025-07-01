0822952acc17eac84e0e21ef38e0b15a2ac07bcb
N2Y <aoaki202320@gmail.com>
2025-07-01 17:09:06 +0900

    schema.rbの修正

--- db/schema.rb
+++ db/schema.rb
@@ -671,6 +671,7 @@ ActiveRecord::Schema.define(version: 2025_06_30_021256) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "user_notifications", "notifications", name: "fk_user_notifications_notifications", on_delete: :cascade
   add_foreign_key "user_notifications", "users", name: "fk_user_notifications_users"

ffe9ff01c8355120a1c3ee570db9e8335caeab2a
N2Y <aoaki202320@gmail.com>
2025-07-03 14:09:06 +0900

    refs #114

---
+++ test/tasks/notification_task_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+require 'rake'
+
+class NotificationTaskTest < ActiveSupport::TestCase
+  def setup
+    Notification.destroy_all
+    @deleted_user = User.last
+    @deleted_user.update!(deleted: true)
+    @user_count = User.where(deleted: false).count
+    Rake.application.rake_require('tasks/notification')
+    Rake::Task.define_task(:environment)
+    Rake::Task['notification:generate'].reenable
+  end
+
+  def test_generate
+    assert_difference 'Notification.count', 1 do
+      assert_difference 'UserNotification.count', @user_count do
+        Rake::Task['notification:generate'].invoke
+      end
+    end
+
+    notification = Notification.last
+    assert_equal "令和7年の算定基礎届の提出期限は 7月10日 です。", notification.message
+    refute_includes notification.users.pluck(:id), @deleted_user.id
+  end
+
+  def test_cleanup
+    Rake::Task['notification:generate'].invoke
+
+    assert_equal 1, Notification.count
+    assert_equal @user_count, UserNotification.count
+
+    Rake::Task['notification:cleanup'].invoke
+    assert_not Notification.exists?
+    assert_not UserNotification.exists?
+  end
+
+end

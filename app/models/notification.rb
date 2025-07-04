b20e387df233dfd6f0ac13c7477b5624078d01aa
N2Y <aoaki202320@gmail.com>
2025-07-04 19:00:40 +0900

    refs #115 formatted_messageをヘルパーメソッドに変更

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,10 +1,4 @@
 class Notification < ApplicationRecord
   has_many :user_notifications, dependent: :destroy
   has_many :users, through: :user_notifications
-
-  def formatted_message
-    message.gsub(/(\d{1,2}月\d{1,2}日)/) do |match| 
-      ActionController::Base.helpers.content_tag(:span, match, style: 'color: red')
-    end.html_safe
-  end
 end
--- test/models/notification_test.rb
+++ test/models/notification_test.rb
@@ -1,11 +1,6 @@
 require 'test_helper'
 
 class NotificationTest < ActiveSupport::TestCase
-  def test_formatted_message
-    n = Notification.first
-    assert_equal "令和7年の算定基礎届の提出期限は <span style=\"color: red\">7月10日</span> です。", n.formatted_message
-  end
-
   def test_dependent_destroy
     n = Notification.first
     assert n.user_notifications.size > 0

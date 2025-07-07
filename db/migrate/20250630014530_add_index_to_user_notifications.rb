4d0f6a33214982d4ad2dd1dd18d175f510425fc2
N2Y <aoaki202320@gmail.com>
2025-07-07 12:03:52 +0900

    refs #117 user_notificationsテーブルと外部キーの存在チェックを削除

--- db/migrate/20250630014530_add_index_to_user_notifications.rb
+++ db/migrate/20250630014530_add_index_to_user_notifications.rb
@@ -1,21 +1,9 @@
 class AddIndexToUserNotifications < ActiveRecord::Migration[6.1]
   def up
-    unless table_exists?(:user_notifications)
-      raise "user_notificationsテーブルが存在しません。"
-    end
-
-    unless index_exists?(:user_notifications, [:user_id, :notification_id], unique: true, name: 'index_user_notifications_on_user_id_and_notification_id')
-      add_index :user_notifications, [:user_id, :notification_id], unique: true, name: 'index_user_notifications_on_user_id_and_notification_id'
-    end
+    add_index :user_notifications, [:user_id, :notification_id], unique: true, name: 'index_user_notifications_on_user_id_and_notification_id'
   end
 
   def down
-    unless table_exists?(:user_notifications)
-      raise "user_notificationsテーブルが存在しません。"
-    end
-
-    if index_exists?(:user_notifications, name: 'index_user_notifications_on_user_id_and_notification_id')
-      remove_index :user_notifications, name: 'index_user_notifications_on_user_id_and_notification_id'
-    end
+    remove_index :user_notifications, name: 'index_user_notifications_on_user_id_and_notification_id'
   end
 end
--- db/migrate/20250630021256_add_foreign_keys_to_user_notifications.rb
+++ db/migrate/20250630021256_add_foreign_keys_to_user_notifications.rb
@@ -1,29 +1,11 @@
 class AddForeignKeysToUserNotifications < ActiveRecord::Migration[6.1]
   def up
-    unless table_exists?(:user_notifications)
-      raise "user_notificationsテーブルが存在しません。"
-    end
-
-    unless foreign_key_exists?(:user_notifications, name: 'fk_user_notifications_users')
-      add_foreign_key :user_notifications, :users, column: :user_id, name: 'fk_user_notifications_users'
-    end
-
-    unless foreign_key_exists?(:user_notifications, name: 'fk_user_notifications_notifications')
-      add_foreign_key :user_notifications, :notifications, column: :notification_id, name: 'fk_user_notifications_notifications', on_delete: :cascade
-    end
+    add_foreign_key :user_notifications, :users, column: :user_id, name: 'fk_user_notifications_users'
+    add_foreign_key :user_notifications, :notifications, column: :notification_id, name: 'fk_user_notifications_notifications', on_delete: :cascade
   end
 
   def down
-    unless table_exists?(:user_notifications)
-      raise "user_notificationsテーブルが存在しません。"
-    end
-
-    if foreign_key_exists?(:user_notifications, name: 'fk_user_notifications_users')
-      remove_foreign_key :user_notifications, name: 'fk_user_notifications_users'
-    end
-
-    if foreign_key_exists?(:user_notifications, name: 'fk_user_notifications_notifications')
-      remove_foreign_key :user_notifications, name: 'fk_user_notifications_notifications'
-    end
+    remove_foreign_key :user_notifications, name: 'fk_user_notifications_users'
+    remove_foreign_key :user_notifications, name: 'fk_user_notifications_notifications'
   end
 end

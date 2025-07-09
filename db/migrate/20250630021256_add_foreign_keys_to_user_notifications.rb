9b5a757ebf09cf3408e876353104940f7d55b6a8
N2Y <aoaki202320@gmail.com>
2025-07-09 15:48:37 +0900

    refs #117 外部キー追加時の存在チェックを追加

--- db/migrate/20250630021256_add_foreign_keys_to_user_notifications.rb
+++ db/migrate/20250630021256_add_foreign_keys_to_user_notifications.rb
@@ -1,6 +1,9 @@
 class AddForeignKeysToUserNotifications < ActiveRecord::Migration[6.1]
   def up
-    add_foreign_key :user_notifications, :users, column: :user_id, name: 'fk_user_notifications_users'
+    unless foreign_key_exists?(:user_notifications, name: 'fk_user_notifications_users')
+      add_foreign_key :user_notifications, :users, column: :user_id, name: 'fk_user_notifications_users'
+    end
+
     add_foreign_key :user_notifications, :notifications, column: :notification_id, name: 'fk_user_notifications_notifications', on_delete: :cascade
   end
 

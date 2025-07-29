d001c16c5f3e738ce4a717e7048852495492b60d
ichy <ichylinux@gmail.com>
2025-07-30 02:19:54 +0900

    リファクタリング

--- vendor/gems/rails-csv-fixtures/lib/rails-csv-fixtures.rb
+++
@@ -1,7 +0,0 @@
-require 'rails-csv-fixtures/version.rb'
-require 'rails-csv-fixtures/railtie.rb'
-require 'rails-csv-fixtures/active_record_csv_fixtures'
-require 'rails-csv-fixtures/active_support_test_case_patch'
-
-module RailsCsvFixtures
-end
--- vendor/gems/rails-csv-fixtures/lib/rails-csv-fixtures/version.rb
+++
@@ -1,3 +0,0 @@
-module RailsCsvFixtures
-  VERSION = "0.0.2"
-end

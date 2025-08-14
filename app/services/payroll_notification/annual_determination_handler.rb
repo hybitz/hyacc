830d356916d09bfcaa77ac518bebf53848d5f1b3
N2Y <aoaki202320@gmail.com>
2025-08-14 11:18:27 +0900

    refs #125 二重否定の箇所を修正

--- app/services/payroll_notification/annual_determination_handler.rb
+++ app/services/payroll_notification/annual_determination_handler.rb
@@ -12,9 +12,9 @@ module PayrollNotification
     end
 
     def execute
-      monthly_standard_unchanged = @payroll.monthly_standard == @pr_1.monthly_standard
-      return if overlaps_with_ad_hoc_revision? || refresh_existing_notification(monthly_standard_unchanged)
-      return if !monthly_standard_unchanged || !annual_determination_pending?
+      monthly_standard_changed = @payroll.monthly_standard != @pr_1.monthly_standard
+      return if overlaps_with_ad_hoc_revision? || refresh_existing_notification(monthly_standard_changed)
+      return if monthly_standard_changed || annual_determination_applied?
       create_notification_and_user_notifications
     end
 
@@ -27,17 +27,17 @@ module PayrollNotification
       Notification.where(employee_id: @employee.id, category: :ad_hoc_revision, ym: target_ym_for_ad_hoc_revision).exists?
     end
 
-    def refresh_existing_notification(monthly_standard_unchanged)
+    def refresh_existing_notification(monthly_standard_changed)
       notification = Notification.find_by(employee_id: @employee.id, category: :annual_determination, ym: @ym)
       if notification
-        should_be_deleted = !monthly_standard_unchanged
+        should_be_deleted = monthly_standard_changed
         notification.update!(deleted: should_be_deleted) unless notification.deleted? == should_be_deleted
         return true
       end
       false
     end
   
-    def annual_determination_pending?
+    def annual_determination_applied?
       base_year = @ym /100
       target_ym_for_annnual_determination = [3, 4, 5].map{|month| base_year * 100 + month} 
       month_data = target_ym_for_annnual_determination.map do |ym|
@@ -58,12 +58,12 @@ module PayrollNotification
       elsif x1 == employment_ym || Date.new(x2/100, x2%100, 1) == employment_date
         bs = pr2.salary_subtotal
       else
-        return false
+        return true
       end
       
       prefecture_code = @employee.business_office.prefecture_code
       new_monthly_standard = TaxUtils.get_basic_info(@ym, prefecture_code, bs).monthly_standard
-      @payroll.monthly_standard != new_monthly_standard
+      @payroll.monthly_standard == new_monthly_standard
     end
 
     def create_notification_and_user_notifications

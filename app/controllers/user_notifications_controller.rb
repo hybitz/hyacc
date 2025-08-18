d2efd68d617a575f54f9cae0ae550b484d874385
N2Y <aoaki202320@gmail.com>
2025-08-18 18:26:52 +0900

    refs #142 Ajaxで1件ずつ更新に変更

--- app/controllers/user_notifications_controller.rb
+++ app/controllers/user_notifications_controller.rb
@@ -1,49 +1,6 @@
 class UserNotificationsController < Base::HyaccController
   
   def index
-    load_notifications_and_user_notifications
-  end
-
-  def update_visibility_settings
-    errors = []
-    missing_count = 0
-
-    un_params.each do |id, attrs|
-      un = find_user_notification(id)
-
-      if un.nil?
-        missing_count += 1
-        next
-      end
-
-      visible = attrs[:visible]
-      next if un.visible == visible
-
-      begin
-        un.update!(visible: visible)
-      rescue => e
-        handle(e)
-        err_msg = flash[:notice].presence || '予期せぬエラーが発生しました。'
-        errors << build_update_error_message(un, err_msg)
-      end
-    end
-
-    errors << build_missing_count_message(missing_count, errors.any?) if missing_count > 0
-  
-    if errors.any?
-      flash[:notice] = errors.join("<br/>")
-      flash[:is_error_message] = true
-      load_notifications_and_user_notifications
-      render 'index'
-    else
-      flash[:notice] = 'お知らせの表示設定を更新しました。'
-      render 'common/reload'
-    end
-  end
-
-  private
-
-  def load_notifications_and_user_notifications
     @deleted_notifications = Notification
       .joins(:user_notifications)
       .where(deleted: true, user_notifications: {user_id: current_user.id})
@@ -55,29 +12,24 @@ class UserNotificationsController < Base::HyaccController
       .order('notifications.created_at DESC')
   end
 
-  def un_params
-    raw = params.require(:user_notifications)
-    ids = raw.keys
-    permitted = raw.permit(ids.index_with {[:visible]}).to_h
-    permitted.transform_values do |attrs|
-      { visible: ActiveModel::Type::Boolean.new.cast(attrs[:visible]) }
+  def update
+    un = current_user.user_notifications
+      .joins(:notification)
+      .where(notifications: { deleted: false })
+      .find_by(id: params[:id])
+
+    return head :unprocessable_entity if un.nil?
+
+    if un.update(user_notification_params)
+      render json: { message: 'ok' }
+    else
+      head :unprocessable_entity
     end
   end
 
-  def find_user_notification(id)
-    current_user.user_notifications
-      .joins(:notification)
-      .where(notifications: {deleted: false})
-      .find_by(id: id)
-  end
+  private
 
-  def build_update_error_message(un, err_msg)
-    title = un.notification.message.truncate(10)
-    "「#{title}」の更新失敗：#{err_msg}"
-  end
-
-  def build_missing_count_message(count, has_other_errors)
-    prefix = has_other_errors ? "その他" : ""
-    "#{prefix}#{count}件の更新失敗：削除済みまたは更新対象外のため"
+  def user_notification_params
+    {visible: ActiveModel::Type::Boolean.new.cast(params.require(:user_notification).permit(:visible)[:visible])}
   end
 end
--- test/controllers/user_notifications_controller_test.rb
+++ test/controllers/user_notifications_controller_test.rb
@@ -24,132 +24,7 @@ class UserNotificationsControllerTest < ActionController::TestCase
     assert_not_empty assigns(:deleted_notifications)
   end
 
-  def test_一件のレコードを更新する
-    sign_in user
-    un =  user.user_notifications
-    .joins(:notification)
-    .where(visible: true, notifications: {deleted: false}).first
-
-    patch :update_visibility_settings, xhr: true, params: {
-      user_notifications: {
-        un.id => {visible: false}
-      }
-    }
-
-    assert_response :success
-    assert_template 'common/reload'
-    assert_equal @response.media_type, 'text/javascript'
-    assert_equal 'お知らせの表示設定を更新しました。', flash[:notice]
-    assert_not un.reload.visible?
-  end
-
-  def test_複数のレコードを更新する
-    UserNotification.delete_all
-    Notification.delete_all
-    ids = [2023, 2024, 2021, 2022].map do |year|
-      notification = Notification.create!(message: year, deleted: false, created_at: Date.new(year, 6, 1), category: :report_submission)
-      user.user_notifications.create!(notification: notification).id
-    end
-
-    sign_in user
-
-    patch :update_visibility_settings, xhr: true, params: {
-      user_notifications: {
-        ids[0] => {visible: false},
-        ids[1] => {visible: true},
-        ids[2] => {visible: false},
-        ids[3] => {visible: true}
-      }
-    }
-
-    assert_response :success
-    assert_template 'common/reload'
-    assert_equal @response.media_type, 'text/javascript'
-    assert_equal "お知らせの表示設定を更新しました。", flash[:notice]
-    assert_equal [[ids[0], false], [ids[1],true], [ids[2] ,false], [ids[3], true]], UserNotification.where(id: ids).pluck(:id, :visible)
-  end
-
-  def test_他のユーザのお知らせを更新しようとするとエラーになる
-    user2 = User.second
-    sign_in user2
-
-    un =  user.user_notifications
-    .joins(:notification)
-    .where(visible: true, notifications: {deleted: false}).first
-
-    patch :update_visibility_settings, xhr: true, params: {
-      user_notifications: {
-        un.id => {visible: false}
-      }
-    }
-
-    assert_response :success
-    assert_template 'index'
-    assert_equal @response.media_type, 'text/javascript'
-    assert_equal "1件の更新失敗：削除済みまたは更新対象外のため", flash[:notice]
-    assert un.reload.visible?
-  end
-
-  def test_更新時のループ処理は例外が発生しても継続する
-    sign_in user
-    n = Notification.create!(deleted: false, category: :ad_hoc_revision)
-    user.user_notifications.create!(notification: n)
-
-    fail_un, ok_un =  user.user_notifications
-    .joins(:notification)
-    .where(visible: true, notifications: {deleted: false}).limit(2).to_a
-    fail_un_id = fail_un.id
-    ok_un_id   = ok_un.id
-
-    ids = []
-    base_id = UserNotification.maximum(:id).to_i
-
-    3.times do |i|
-      id = base_id + i + 1
-      assert_nil UserNotification.find_by(id: id)
-      ids << id
-    end
-
-    non_existing_id1, non_existing_id2, non_existing_id3 = ids
-    missing_count = ids.size
-
-    UserNotification.class_eval do
-      alias_method :original_update!, :update!
-    
-      define_method :update! do |*args|
-        if self.id == fail_un_id
-          raise ActiveRecord::StaleObjectError
-        else
-          original_update!(*args)
-        end
-      end
-    end
-    
-    patch :update_visibility_settings, xhr: true, params: {
-      user_notifications: {
-        non_existing_id1 => {visible: false},
-        fail_un_id => {visible: false},
-        non_existing_id2 => {visible: false},
-        ok_un_id => {visible: false},
-        non_existing_id3 => {visible: false}
-      }
-    }
-
-    assert_response :success
-    assert_template 'index'
-    assert_equal @response.media_type, 'text/javascript'
-    assert_equal "「令和7年の算定...」の更新失敗：古いデータを更新しようとしました。<br/>その他#{missing_count}件の更新失敗：削除済みまたは更新対象外のため", flash[:notice] 
-    
-    assert fail_un.reload.visible?
-    assert_not ok_un.reload.visible?
-
-    UserNotification.class_eval do
-      alias_method :update!, :original_update!
-      remove_method :original_update!
-    end 
-  end
-
-  def test_load_users_and_user_notifications
+  def test_レコードはcreated_atの降順で表示する
     UserNotification.delete_all
     Notification.delete_all
 
@@ -182,4 +57,35 @@ class UserNotificationsControllerTest < ActionController::TestCase
     user_notifications = assigns(:user_notifications)
     assert_equal ["2024", "2023", "2022", "2021"], user_notifications.map {|un| un.notification.message}
   end
+
+  def test_更新
+    sign_in user
+
+    user_notification = UserNotification.first
+    
+    assert_changes 'user_notification.reload.visible' do
+      patch :update, xhr: true, params: {id: user_notification.id,
+        user_notification: {visible: false}
+      }
+    end
+
+    assert_response :success
+    assert_not user_notification.reload.visible?
+  end
+
+  def test_更新_エラー
+    sign_in user
+
+    user_notification = UserNotification.first
+    user_notification.notification.update!(deleted: true)
+
+    assert_no_changes 'user_notification.reload.visible' do
+      patch :update, xhr: true, params: {id: user_notification.id,
+      user_notification: {visible: false}
+      }
+    end
+
+    assert_response :unprocessable_entity
+  end
+
 end

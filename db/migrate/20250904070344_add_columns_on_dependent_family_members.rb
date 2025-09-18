25636cfa6fb8de57dc9e0749b2ccc4ca46904f0e
N2Y <aoaki202320@gmail.com>
2025-09-18 12:25:29 +0900

    refs #157 dependent_family_membersテーブルのカラム追加マイグレーションをup/downメソッドに変更

--- db/migrate/20250904070344_add_columns_on_dependent_family_members.rb
+++ db/migrate/20250904070344_add_columns_on_dependent_family_members.rb
@@ -1,8 +1,15 @@
 class AddColumnsOnDependentFamilyMembers < ActiveRecord::Migration[6.1]
-  def change
+  def up
     change_table :dependent_family_members, bulk: true do |t|
       t.integer :family_sub_type
       t.string  :non_resident_code
+    end
+  end
+
+  def down
+    change_table :dependent_family_members, bulk: true do |t|
+      t.remove :family_sub_type
+      t.remove :non_resident_code
     end
   end
 end

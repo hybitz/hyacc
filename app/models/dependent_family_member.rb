a707a2cfd827f0ad7bb3aef2026cf8210204f7a3
ichy <ichylinux@gmail.com>
2025-10-02 22:41:33 +0900

    refs #157 リファクタリング

--- app/models/dependent_family_member.rb
+++ app/models/dependent_family_member.rb
@@ -2,13 +2,18 @@ class DependentFamilyMember < ApplicationRecord
   belongs_to :exemption
   validates_presence_of :name, :kana
   validates :family_sub_type, presence: true, if: -> {exemption_type == 2}
-  validates :non_resident_code, presence: true, if: -> {exemption_type == 2 && live_in == false}
-
-  validates_with Validators::NonResidentCodeValidator
-  validates_with Validators::ExemptionTypeRestrictionValidator
+  validates :non_resident_code, presence: true, if: -> {exemption_type == 2 && non_resident?}
 
   before_validation do
     self.non_resident_code = nil if non_resident_code.blank?
     self.family_sub_type   = nil if family_sub_type.blank?
   end
+
+  validates_with Validators::NonResidentCodeValidator
+  validates_with Validators::ExemptionTypeRestrictionValidator
+
+  def non_resident?
+    !live_in?
+  end
+
 end

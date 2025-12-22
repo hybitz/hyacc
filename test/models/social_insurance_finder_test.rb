55108e778b21c8c1aeb01db5591e7ababfdcd97f
N2Y <aoaki202320@gmail.com>
2025-12-22 17:55:08 +0900

    refs #193 リファクタリング: テストケース内の被保険者整理番号の設定をフィクスチャに移行

--- test/models/social_insurance_finder_test.rb
+++ test/models/social_insurance_finder_test.rb
@@ -4,27 +4,21 @@ class SocialInsuranceFinderTest < ActiveSupport::TestCase
   def setup
     @finder = SocialInsuranceFinder.new(company_id: 1, fiscal_year: 2026)
 
-    @employee1 = Employee.first
-    @employee2 = Employee.second
-    @employee3 = Employee.find(6)
-
-    @employee3.update!(social_insurance_reference_number: 1)
-    @employee1.update!(social_insurance_reference_number: 10)
-    @employee2.update!(social_insurance_reference_number: nil)
-
-    Payroll.create!(employee: @employee1, ym: 202601, pay_day: "2026-02-25",
+    employee1 = Employee.first
+    employee2 = Employee.second
+    employee3 = Employee.find(6)
+    Payroll.create!(employee: employee1, ym: 202601, pay_day: "2026-02-25",
       base_salary: 300000, create_user_id: 1, update_user_id: 1)
-
-    Payroll.create!(employee: @employee2, ym: 202601, pay_day: "2026-02-25",
+    Payroll.create!(employee: employee2, ym: 202601, pay_day: "2026-02-25",
       base_salary: 300000, create_user_id: 1, update_user_id: 1)
-
-    Payroll.create!(employee: @employee3, ym: 202601, pay_day: "2026-02-25",
+    Payroll.create!(employee: employee3, ym: 202601, pay_day: "2026-02-25",
       base_salary: 300000, create_user_id: 1, update_user_id: 1)
   end
 
   def test_employees_are_sorted_by_social_insurance_reference_number
     result = @finder.list_payrolls_by_employee.map(&:first)
-
-    assert_equal [@employee3, @employee1, @employee2], result
+    reference_numbers = result.map(&:social_insurance_reference_number)
+    expected_order = reference_numbers.compact.sort + [nil] * reference_numbers.count(&:nil?)
+    assert_equal expected_order, reference_numbers
   end
 end

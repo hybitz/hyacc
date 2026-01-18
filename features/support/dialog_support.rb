e8e04e5e39c42b321fea89c3806f2864aac907fe
ichylinux <ichylinux@gmail.com>
2026-01-18 14:51:35 +0900

    リファクタリング

--- features/support/dialog_support.rb
+++ features/support/dialog_support.rb
@@ -1,24 +1,24 @@
 module DialogSupport
 
   def has_dialog?(options = {})
-    options = {:title => options} if options.is_a?(String) or options.is_a?(Regexp)
+    options = {title: options} if options.is_a?(String) || options.is_a?(Regexp)
 
-    assert wait_until { has_selector?("div.ui-dialog", visible: true) }
+    assert_selector("div.ui-dialog", visible: true)
 
     if options[:title]
-      has_selector?("span.ui-dialog-title", :text => options[:title])
+      assert_selector("span.ui-dialog-title", text: options[:title])
     end
 
     true
   end
 
   def has_no_dialog?(options = {})
-    options = {title: options} if options.is_a?(String) or options.is_a?(Regexp)
+    options = {title: options} if options.is_a?(String) || options.is_a?(Regexp)
 
-    assert wait_until { has_no_selector?("div.ui-dialog") }
+    assert_no_selector("div.ui-dialog")
 
     if options[:title]
-      has_no_selector?("span.ui-dialog-title", text: options[:title])
+      assert_no_selector("span.ui-dialog-title", text: options[:title])
     end
 
     true

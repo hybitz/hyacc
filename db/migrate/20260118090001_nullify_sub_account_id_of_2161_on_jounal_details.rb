63c494c6c0ba607db280d48c21e19e9e3363c9e7
ichylinux <ichylinux@gmail.com>
2026-01-18 17:13:57 +0900

    工具器具備品で補助科目に不正に値が入っている過去データがあったので訂正

---
+++ db/migrate/20260118090001_nullify_sub_account_id_of_2161_on_jounal_details.rb
@@ -0,0 +1,12 @@
+class NullifySubAccountIdOf2161OnJounalDetails < ActiveRecord::Migration[7.2]
+  def up
+    a = Account.find_by(code: '2161')
+    raise '想定している工具器具備品ではありません。' unless a.name == '工具器具備品'
+    raise '現在は補助科目が登録されています' unless a.sub_accounts.empty?
+    
+    JournalDetail.where('sub_account_id is not null').update_all(['sub_account_id = ?', nil])
+  end
+
+  def down
+  end
+end

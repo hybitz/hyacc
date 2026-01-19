5437fb22277fc18b1d49ad8874d11d39c34823b6
ichylinux <ichylinux@gmail.com>
2026-01-19 17:13:51 +0900

    マイグレーション不備を修正

--- db/migrate/20260118090001_nullify_sub_account_id_of_2161_on_jounal_details.rb
+++ db/migrate/20260118090001_nullify_sub_account_id_of_2161_on_jounal_details.rb
@@ -4,7 +4,7 @@ class NullifySubAccountIdOf2161OnJounalDetails < ActiveRecord::Migration[7.2]
     raise '想定している工具器具備品ではありません。' unless a.name == '工具器具備品'
     raise '現在は補助科目が登録されています' unless a.sub_accounts.empty?
     
-    JournalDetail.where('sub_account_id is not null').update_all(['sub_account_id = ?', nil])
+    JournalDetail.where('account_id = ? and sub_account_id is not null', a.id).update_all(['sub_account_id = ?', nil])
   end
 
   def down

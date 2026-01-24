822c4ef08ec3819623bbb00fa0dc7036b64d1bc8
ichy <ichylinux@gmail.com>
2026-01-25 02:14:56 +0900

    Flakyなテストを修正

--- features/support/dialog_support.rb
+++ features/support/dialog_support.rb
@@ -15,7 +15,13 @@ module DialogSupport
   def has_no_dialog?(options = {})
     options = {title: options} if options.is_a?(String) || options.is_a?(Regexp)
 
-    assert wait_until { has_no_selector?("div.ui-dialog") }
+    assert wait_until do
+      begin
+        has_no_selector?("div.ui-dialog")
+      rescue Selenium::WebDriver::Error::UnknownError => e
+        e.message.include?('Node with given id does not belong to the document')
+      end
+    end
 
     if options[:title]
       assert_no_selector("span.ui-dialog-title", text: options[:title])

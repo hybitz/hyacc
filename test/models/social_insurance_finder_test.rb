ec49b48b11a271d8d28a255c28be094078149c73
N2Y <aoaki202320@gmail.com>
2026-04-14 19:46:04 +0900

    refs #299 テスト: social_insurance_finderの検証方法を見直し干渉を解消

--- test/models/social_insurance_finder_test.rb
+++ test/models/social_insurance_finder_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class SocialInsuranceFinderTest < ActiveSupport::TestCase
   def setup
-    @finder = SocialInsuranceFinder.new(company_id: 1, fiscal_year: 2026)
+    @finder = SocialInsuranceFinder.new(company_id: 1, fiscal_year: 2027)
 
     [1, 2, 6].each do |employee_id|
       Payroll.create!(
         employee_id: employee_id,
-        ym: 202601,
-        pay_day: "2026-02-25",
+        ym: 202701,
+        pay_day: "2027-02-25",
         base_salary: 300000,
         create_user_id: 1,
         update_user_id: 1
@@ -18,8 +18,6 @@ class SocialInsuranceFinderTest < ActiveSupport::TestCase
 
   def test_employees_are_sorted_by_social_insurance_reference_number
     result = @finder.list_payrolls_by_employee.map(&:first)
-    target_employee_ids = [1, 2, 6]
-    result = result.select { |employee| target_employee_ids.include?(employee.id) }
     expected_employee_ids = [6, 1, 2]
     expected_reference_numbers = [1, 10, nil]
     
